caaede32c80022623cd5095ea128e61a6eebc223
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-05-05 09:00:04 +0700

    Time: 3 ms (92.47%), Space: 11.5 MB (5.44%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,6 +9,7 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         while(1) {
             node->val = node->next->val;
             if(node->next->next) node = node->next;
