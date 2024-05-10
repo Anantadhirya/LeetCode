8c2bc57b910197205cd901b9ad93ae8d3c30586f
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-05-10 08:42:34 +0700

    Time: 309 ms (46.81%), Space: 120.7 MB (25.53%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         vector<pair<double, pair<int, int>>> v;
         int n = arr.size();
         vector<int> ans(2);
