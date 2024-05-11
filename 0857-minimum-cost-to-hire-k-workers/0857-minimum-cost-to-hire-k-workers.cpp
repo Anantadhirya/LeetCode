af66cdcf104833a3b29b2c164f8e4fa1a2590a34
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-05-11 07:59:46 +0700

    Time: 23 ms (80.17%), Space: 24.8 MB (86.87%) - LeetHub

---
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+        int n = quality.size();
+        vector<pair<double, int>> v(n);
+        
+        for(int i = 0; i < n; i++) {
+            v[i] = {1.0*wage[i]/quality[i], quality[i]};
+        }
+        sort(v.begin(), v.end());
+        
+        priority_queue<int> pq;
+        int smQ = 0;
+        double ans = 1/0.0;
+        for(const auto &[r, q]: v) {
+            pq.push(q);
+            smQ += q;
+            while(pq.size() > k) {
+                smQ -= pq.top();
+                pq.pop();
+            }
+            if(pq.size() == k) {
+                ans = min(ans, r * smQ);
+            }
+        }
+        return ans;
+    }
+};
