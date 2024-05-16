750ae3e1bd117840d3a4637e9fb8ad60ec9badfa
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-05-16 11:54:14 +0700

    Time: 10 ms (63.78%), Space: 17.6 MB (24.21%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
@@ -11,11 +11,15 @@
  */
 class Solution {
 public:
-    bool evaluateTree(TreeNode* root) {
-        int l = root->left ? evaluateTree(root->left) : (root->val == 3);
-        int r = root->right ? evaluateTree(root->right) : (root->val == 3);
+    bool solve(TreeNode* root) {
+        int l = root->left ? solve(root->left) : (root->val == 3);
+        int r = root->right ? solve(root->right) : (root->val == 3);
         if(root->val == 2) return l || r;
         if(root->val == 3) return l && r;
         return root->val;
+    }
+    bool evaluateTree(TreeNode* root) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
+        return solve(root);
     }
 };
