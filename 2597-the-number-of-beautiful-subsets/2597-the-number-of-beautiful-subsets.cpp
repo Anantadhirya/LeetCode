013c54a10a9bd388dfdee4a30987ec31bed2ec47
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-05-23 08:48:00 +0700

    Time: 134 ms (66.12%), Space: 43.6 MB (8.06%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void solve(int curr, int &n, int &k, vector<int> &m, vector<int> &nums, int &ans) {
+        if(curr == n) {
+            ans++;
+            return;
+        }
+        solve(curr+1, n, k, m, nums, ans);
+        if(nums[curr] - k < 1 || m[nums[curr] - k] == 0) {
+            m[nums[curr]]++;
+            solve(curr+1, n, k, m, nums, ans);
+            m[nums[curr]]--;
+        }
+    }
+    int beautifulSubsets(vector<int>& nums, int k) {
+        int n = nums.size(), ans = 0;
+        vector<int> m(1000+5, 0);
+        sort(nums.begin(), nums.end());
+        solve(0, n, k, m, nums, ans);
+        return ans - 1;
+    }
+};
