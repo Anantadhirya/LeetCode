7bd199c6eb4ffc644c5a015b4449d55c02eab284
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-05-23 08:48:16 +0700

    Time: 140 ms (65.91%), Space: 43.5 MB (8.06%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -13,6 +13,7 @@ public:
         }
     }
     int beautifulSubsets(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = nums.size(), ans = 0;
         vector<int> m(1000+5, 0);
         sort(nums.begin(), nums.end());
