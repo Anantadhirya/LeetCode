9ae2009cda2286e0a4967c7dd26548ab21666024
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-05-26 08:11:00 +0700

    Time: 1005 ms (36.33%), Space: 449.1 MB (18.94%) - LeetHub

---
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int checkRecord(int n) {
+        const long long MOD = 1e9 + 7;
+        vector<vector<vector<long long>>> dp(n+1, vector<vector<long long>>(2, vector<long long>(3, 0)));
+        long long ans = 0;
+        dp[0][0][0] = 1;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 2; j++) {
+                for(int k = 0; k < 3; k++) {
+                    // Case P
+                    (dp[i+1][j][0] += dp[i][j][k]) %= MOD;
+                    
+                    // Case A
+                    if(j == 0) {
+                        (dp[i+1][j+1][0] += dp[i][j][k]) %= MOD;
+                    }
+                    
+                    // Case L
+                    if(k != 2) {
+                        (dp[i+1][j][k+1] += dp[i][j][k]) %= MOD;
+                    }
+                }
+            }
+        }
+        for(int j = 0; j < 2; j++) {
+            for(int k = 0; k < 3; k++) {
+                (ans += dp[n][j][k]) %= MOD;
+            }
+        }
+        return ans;
+    }
+};
