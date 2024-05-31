fbdf9e7b2872e4ec3b1bb0707ca4b792eac49b3a
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-05-31 08:31:32 +0700

    Time: 3 ms (96.61%), Space: 14.6 MB (10.01%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         vector<int> ans;
         unordered_map<int, int> cnt;
         for(const auto &i: nums) {
