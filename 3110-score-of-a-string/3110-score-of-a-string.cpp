18d44ab619f03c9af368b65d75fdae00010e9fb9
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-06-01 08:51:24 +0700

    Time: 0 ms (100.00%), Space: 8 MB (7.39%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int scoreOfString(string s) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = s.size(), ans = 0;
         for(int i = 1; i < n; i++) {
             ans += abs((int)s[i]-s[i-1]);
