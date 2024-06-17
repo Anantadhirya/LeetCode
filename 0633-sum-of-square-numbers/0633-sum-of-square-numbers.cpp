6f2d8f4f3344558301b792e02480f2cd7f890c27
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-06-17 09:50:37 +0700

    Time: 5 ms (34.50%), Space: 7.2 MB (18.45%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         for(long long a = 0, b; a*a <= c; a++) {
             b = sqrt(c - a*a);
             if(a*a + b*b == c) return 1;
