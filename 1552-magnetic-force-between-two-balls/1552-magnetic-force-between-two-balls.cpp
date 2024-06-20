e64879215aab2fb5176fe21481a4ba9062891be2
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-06-20 08:30:18 +0700

    Time: 84 ms (98.88%), Space: 61.2 MB (95.31%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxDistance(vector<int>& position, int m) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = position.size();
         sort(position.begin(), position.end());
         int l = 0, r = position.back() - position[0], mid;
