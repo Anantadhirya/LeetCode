0f14953eaf97ea05a7fdd937e996848eb85f6366
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-06-21 08:58:32 +0700

    Time: 7 ms (99.89%), Space: 34.5 MB (11.51%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -3,18 +3,16 @@ public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         ios_base::sync_with_stdio(false); cin.tie(0);
         int n = customers.size();
-        int ans = 0, tmp = 0;
+        int ans = 0, tmp = 0, ans2 = 0;
         for(int i = 0, j = 0; i < n; i++) {
             if(grumpy[i]) tmp += customers[i];
+            else ans2 += customers[i];
             if(i-j >= minutes) {
                 if(grumpy[j]) tmp -= customers[j];
                 j++;
             }
             ans = max(ans, tmp);
         }
-        for(int i = 0; i < n; i++) {
-            if(!grumpy[i]) ans += customers[i];
-        }
-        return ans;
+        return ans + ans2;
     }
 };
