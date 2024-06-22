5bbd30435692e9ecabf6076ffa1100b5e84b54d9
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-06-22 08:57:00 +0700

    Time: 36 ms (99.97%), Space: 74.3 MB (25.30%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         vector<int> v;
         int n = nums.size(), ans = 0;
         v.push_back(-1);
