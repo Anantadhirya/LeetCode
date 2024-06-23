3eef178f2ed9cac1ad70edc03662457a61a47d04
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-06-23 09:17:48 +0700

    Time: 122 ms (44.13%), Space: 81.9 MB (5.74%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = nums.size(), ans = 0;
         multiset<int> s;
         for(int i = 0, j = 0; i < n; i++) {
