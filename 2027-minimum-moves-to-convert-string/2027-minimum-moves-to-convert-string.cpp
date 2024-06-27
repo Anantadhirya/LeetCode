b9b300b8511c90ee2d4df8fad3056eead979b1de
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-06-27 09:50:22 +0700

    Time: 0 ms (100.00%), Space: 7.7 MB (14.36%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minimumMoves(string s) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int ans = 0, n = s.size();
         for(int i = 0; i < n; i++) {
             if(s[i] == 'X') {
