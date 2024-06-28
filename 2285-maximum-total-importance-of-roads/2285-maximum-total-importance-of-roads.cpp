5b251f7a93c3aecdba408b7b7a38ffbadf171e20
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-06-28 10:03:13 +0700

    Time: 212 ms (99.31%), Space: 119.2 MB (73.45%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         vector<int> cnt(n, 0);
         vector<int> v;
         long long ans = 0;
