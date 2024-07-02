7fb3cede059447dd7275ec9c8236e8763a9ba62d
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-02 09:59:36 +0700

    Time: 4 ms (59.42%), Space: 14.4 MB (7.47%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         unordered_map<int, int> cnt;
         for(const auto &i: nums1) {
             cnt[i]++;
