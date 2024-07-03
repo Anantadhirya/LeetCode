037dcaaf32eff3b99683c643c948fd168dff8837
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-03 09:17:37 +0700

    Time: 32 ms (99.61%), Space: 38.9 MB (5.95%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minDifference(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = nums.size();
         if(n <= 4) return 0;
         sort(nums.begin(), nums.end());
