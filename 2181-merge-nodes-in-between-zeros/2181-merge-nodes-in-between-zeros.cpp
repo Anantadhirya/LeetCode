af1bc5e75d1e94e9f3496440848c4eecb7328704
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-04 10:20:23 +0700

    Time: 397 ms (92.73%), Space: 283.9 MB (8.83%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -11,6 +11,7 @@
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         vector<int> v;
         while(head) {
             if(head->val == 0) v.push_back(0);
