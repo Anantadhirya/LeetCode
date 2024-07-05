32e6c8e143211a6a9ba9b9dbf0f75e87efc48904
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-05 09:10:07 +0700

    Time: 106 ms (99.41%), Space: 125.2 MB (5.36%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,6 +11,7 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         vector<int> v;
         while(head) {
             v.push_back(head->val);
