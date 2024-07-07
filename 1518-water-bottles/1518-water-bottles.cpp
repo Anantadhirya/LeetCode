0a5a8c0b9537ac9669cbe9e386ad0a330e3730f6
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-07 09:22:52 +0700

    Time: 0 ms (100.00%), Space: 7.2 MB (30.43%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int ans = numBottles, tmp;
         while(numBottles >= numExchange) {
             tmp = numBottles/numExchange;
