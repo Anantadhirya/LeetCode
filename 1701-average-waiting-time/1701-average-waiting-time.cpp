34dae58a974d8c55cd336f591b4a941c87a8a010
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-09 16:47:53 +0700

    Time: 120 ms (99.12%), Space: 76.8 MB (21.76%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
+        int n = customers.size();
+        int t = 0;
+        double ans = 0;
+        for(int i = 0; i < n; i++) {
+            t = max(t, customers[i][0]) + customers[i][1];
+            ans += t - customers[i][0];
+        }
+        return ans / n;
+    }
+};
