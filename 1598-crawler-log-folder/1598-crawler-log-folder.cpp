96113c84bf76bc9def2114069a224ea8c91b6604
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-10 07:06:53 +0700

    Time: 0 ms (100.00%), Space: 14.1 MB (41.82%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int ans = 0;
         for(const auto &s: logs) {
             if(s == "../") ans = max(ans-1, 0);
