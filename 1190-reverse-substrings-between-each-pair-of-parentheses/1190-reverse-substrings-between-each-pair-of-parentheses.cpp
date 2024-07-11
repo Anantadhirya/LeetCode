89134964f5a21014dfcb40f7578589bf9f5a4a92
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-11 09:10:27 +0700

    Time: 0 ms (100.00%), Space: 11.1 MB (8.54%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -17,6 +17,7 @@ public:
         return s.substr(l, r-l+1);
     }
     string reverseParentheses(string s) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         return solve(0, s.size()-1, s);
     }
 };
