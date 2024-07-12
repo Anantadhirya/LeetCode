1f74364ff8a5090b0a9573f0357d4435e5bd3f20
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-12 09:09:19 +0700

    Time: 83 ms (61.79%), Space: 26 MB (38.62%) - LeetHub

---
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        string s1, s2;
+        string p = (x >= y ? "ab" : "ba");
+        if(y > x) swap(x, y);
+        int ans = 0;
+        for(const char &c: s) {
+            if(!s1.empty() && s1.back() == p[0] && c == p[1]) {
+                ans += x;
+                s1.pop_back();
+            } else s1.push_back(c);
+        }
+        for(const char &c: s1) {
+            if(!s2.empty() && s2.back() == p[1] && c == p[0]) {
+                ans += y;
+                s2.pop_back();
+            } else s2.push_back(c);
+        }
+        return ans;
+    }
+};
