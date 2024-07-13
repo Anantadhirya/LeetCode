fc85ac33a97de864d640570ab86a87b5759bb8aa
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-13 10:34:47 +0700

    Time: 152 ms (99.24%), Space: 204.9 MB (67.56%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = positions.size();
         vector<pair<int, int>> v;
         vector<int> ans, ret;
