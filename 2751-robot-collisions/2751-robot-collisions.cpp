f156baab8f0db8677bf43b0a4c637a72d1a5763d
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-13 10:34:19 +0700

    Time: 214 ms (65.65%), Space: 204.9 MB (67.56%) - LeetHub

---
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        int n = positions.size();
+        vector<pair<int, int>> v;
+        vector<int> ans, ret;
+        for(int i = 0; i < n; i++) {
+            v.push_back({positions[i], i});
+        }
+        sort(v.begin(), v.end());
+        for(auto &[_, i]: v) {
+            while(!ans.empty() && directions[ans.back()] == 'R' && directions[i] == 'L' && healths[i] != 0) {
+                if(healths[ans.back()] == healths[i]) {
+                    healths[ans.back()] = 0;
+                    healths[i] = 0;
+                    ans.pop_back();
+                } else if(healths[ans.back()] < healths[i]) {
+                    healths[ans.back()] = 0;
+                    healths[i]--;
+                    ans.pop_back();
+                } else if(healths[ans.back()] > healths[i]) {
+                    healths[ans.back()]--;
+                    healths[i] = 0;
+                    if(healths[ans.back()] == 0) ans.pop_back();
+                }
+            } 
+            if(healths[i] > 0) ans.push_back(i);
+        }
+        for(int i = 0; i < n; i++) {
+            if(healths[i] != 0) ret.push_back(healths[i]);
+        }
+        return ret;
+    }
+};
