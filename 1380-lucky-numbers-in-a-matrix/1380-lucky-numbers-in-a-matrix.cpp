6b597c174386907284b0a3ab9ceb78d62f2568fb
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-19 10:16:34 +0700

    Time: 17 ms (39.49%), Space: 14.3 MB (27.66%) - LeetHub

---
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        vector<int> colmax(m, 0);
+        int idx;
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                colmax[j] = max(colmax[j], matrix[i][j]);
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            idx = 0;
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] < matrix[i][idx]) idx = j;
+            }
+            if(matrix[i][idx] == colmax[idx]) ans.push_back(matrix[i][idx]);
+        }
+        return ans;
+    }
+};
