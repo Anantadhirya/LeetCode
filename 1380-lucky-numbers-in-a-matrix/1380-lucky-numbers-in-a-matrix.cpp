0ba33a00fd5e65d375dc896d2c1047445ab60657
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-19 10:16:54 +0700

    Time: 10 ms (93.62%), Space: 14.3 MB (27.66%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = matrix.size(), m = matrix[0].size();
         vector<int> colmax(m, 0);
         int idx;
