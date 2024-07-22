13ba91aa773d7b0ec5048c5ae89e654ab7fbea48
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-22 11:22:06 +0700

    Time: 20 ms (92.51%), Space: 24.1 MB (79.25%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = names.size();
         vector<pair<int, string>> v(n);
         for(int i = 0; i < n; i++) {
