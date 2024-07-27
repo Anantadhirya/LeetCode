5b3b41c4725ff32bae1f21c18d2b393704f8e2e4
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-27 08:01:26 +0700

    Time: 152 ms (84.63%), Space: 96.7 MB (76.59%) - LeetHub

---
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        const int inf = INT_MAX;
+        vector<vector<int>> dist(26, vector<int>(26, inf));
+        int n = source.size(), m = original.size();
+        for(int i = 0, u, v, w; i < m; i++) {
+            u = original[i] - 'a', v = changed[i] - 'a', w = cost[i];
+            dist[u][v] = min(dist[u][v], w);
+        }
+        for(int i = 0; i < 26; i++) {
+            dist[i][i] = 0;
+        }
+        for(int k = 0; k < 26; k++) {
+            for(int i = 0; i < 26; i++) {
+                for(int j = 0; j < 26; j++) {
+                    if(dist[i][k] == inf || dist[k][j] == inf) continue;
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+        long long ans = 0;
+        for(int i = 0, u, v; i < n; i++) {
+            u = source[i] - 'a', v = target[i] - 'a';
+            if(dist[u][v] == inf) return -1;
+            ans += (long long)dist[u][v];
+        }
+        return ans;
+    }
+};
