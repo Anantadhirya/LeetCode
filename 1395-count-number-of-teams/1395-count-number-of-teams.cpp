b7cdcba46c927cf418d6b92be29d959cff169ebe
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-29 10:17:05 +0700

    Time: 40 ms (53.85%), Space: 13.2 MB (26.24%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    void update(int x, int &mx, vector<int> &ft) {
-        for(int i = x; i < mx; i += i&-i) {
-            ft[i]++;
-        }
-    }
-    int query(int x, vector<int> &ft) {
-        int ret = 0;
-        for(int i = x; i > 0; i -= i&-i) {
-            ret += ft[i];
-        }
-        return ret;
-    }
     int numTeams(vector<int>& rating) {
         ios_base::sync_with_stdio(false); cin.tie(0);
         int n = rating.size();
-        int mx = *max_element(rating.begin(), rating.end()) + 1;
-        vector<int> ft(mx, 0);
         int ans = 0;
+        vector<vector<int>> dp(n, vector<int>(2, 0));
         for(int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++) {
-                if(rating[i] < rating[j]) {
-                    ans += query(rating[i]-1, ft);
+            for(int j = 0; j < i; j++) {
+                if(rating[j] < rating[i]) {
+                    dp[i][0]++;
+                    ans += dp[j][0];
                 }
-                if(rating[i] > rating[j]) {
-                    ans += i - query(rating[i], ft);
+                if(rating[j] > rating[i]) {
+                    dp[i][1]++;
+                    ans += dp[j][1];
                 }
             }
-            update(rating[i], mx, ft);
         }
         return ans;
     }
