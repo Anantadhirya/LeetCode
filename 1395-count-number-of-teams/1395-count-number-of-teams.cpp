687407e47814fc0d1dbcf3a162edb119366c5976
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-29 10:14:34 +0700

    Time: 70 ms (43.41%), Space: 16.7 MB (17.28%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -13,6 +13,7 @@ public:
         return ret;
     }
     int numTeams(vector<int>& rating) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = rating.size();
         int mx = *max_element(rating.begin(), rating.end()) + 1;
         vector<int> ft(mx, 0);
