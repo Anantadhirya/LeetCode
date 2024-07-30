18deda0af6d4493addb9c60915182208b16fb176
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-30 11:15:33 +0700

    Time: 41 ms (99.04%), Space: 38.5 MB (35.25%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = s.size();
         vector<int> pref(n+1, 0);
         for(int i = 1; i <= n; i++) {
