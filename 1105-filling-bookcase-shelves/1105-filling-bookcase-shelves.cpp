7b583e3f8359fd66bdad0f01057fd296ef2e0df5
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-07-31 10:05:59 +0700

    Time: 5 ms (49.25%), Space: 11 MB (58.66%) - LeetHub

---
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        int n = books.size();
+        const int inf = INT_MAX;
+        vector<int> dp(n+1, inf);
+        dp[0] = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1, t = 0, h = 0; j <= n; j++) {
+                t += books[j-1][0];
+                h = max(h, books[j-1][1]);
+                if(t > shelfWidth) break;
+                dp[j] = min(dp[j], dp[i] + h);
+            }
+        }
+        return dp[n];
+    }
+};
