46b097fd2b1b2848ae705c870a73a9474f9ad7e7
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-08-01 08:23:26 +0700

    Time: 10 ms (18.05%), Space: 17.8 MB (16.71%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int countSeniors(vector<string>& details) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int ans = 0;
         for(const auto &i: details) {
             ans += stoi(i.substr(11, 2)) > 60;
