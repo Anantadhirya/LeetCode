dc05e017cb5a1c061c4c32644a8ef745c3c75940
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-08-02 10:06:31 +0700

    Time: 53 ms (97.84%), Space: 83.3 MB (45.38%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = nums.size();
         int k = 0, cnt = 0, ans = 0;
         for(const auto &i: nums) {
