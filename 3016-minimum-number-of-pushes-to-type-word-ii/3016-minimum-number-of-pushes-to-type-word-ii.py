4a3a0bfed354ac848538b134886244f622fac6ca
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-08-06 07:43:03 +0700

    Time: 145 ms (77.57%), Space: 17.5 MB (73.03%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,8 +1,6 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        cnt = {}
-        for i in word:
-            cnt[i] = cnt.get(i, 0) + 1
+        cnt = Counter(word)
         v = sorted(cnt.values())
         n = len(v)
         ans = 0
