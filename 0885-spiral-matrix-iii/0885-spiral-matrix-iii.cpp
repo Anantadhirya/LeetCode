582a34c5b4905d7cc1603a32568db76c7ccf0d0e
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-08-08 09:37:57 +0700

    Time: 17 ms (5.54%), Space: 14.9 MB (5.29%) - LeetHub

---
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        vector<vector<int>> ans;
+        int n = max(rows, cols);
+        auto add = [&]() {
+            if(0 <= rStart && rStart < rows && 0 <= cStart && cStart < cols) {
+                ans.push_back({rStart, cStart});
+            }
+        };
+        add();
+        for(int i = 1; ans.size() != rows * cols; i += 2) {
+            cStart++;
+            add();
+            for(int j = 1; j <= i; j++) {
+                rStart++;
+                add();
+            }
+            for(int j = 1; j <= i+1; j++) {
+                cStart--;
+                add();
+            }
+            for(int j = 1; j <= i+1; j++) {
+                rStart--;
+                add();
+            }
+            for(int j = 1; j <= i+1; j++) {
+                cStart++;
+                add();
+            }
+        }
+        return ans;
+    }
+};
