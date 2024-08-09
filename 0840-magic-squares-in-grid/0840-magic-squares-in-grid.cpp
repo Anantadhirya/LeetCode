0cb4f7b8ddd41daf5e808555c2670f21b1f7ad2e
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-08-09 10:34:44 +0700

    Time: 7 ms (12.18%), Space: 11.8 MB (24.37%) - LeetHub

---
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        auto check = [&](int i, int j) {
+            vector<int> sr(3, 0), sc(3, 0), d(2, 0);
+            bitset<9> b = 0;
+            for(int ii = 0; ii < 3; ii++) {
+                for(int jj = 0; jj < 3; jj++) {
+                    if(grid[i+ii][j+jj] < 1 || grid[i+ii][j+jj] > 9) return 0;
+                    b[grid[i+ii][j+jj] - 1] = 1;
+                    sr[ii] += grid[i+ii][j+jj];
+                    sc[jj] += grid[i+ii][j+jj];
+                    if(ii == jj) d[0] += grid[i+ii][j+jj];
+                    if(ii == 2-jj) d[1] += grid[i+ii][j+jj];
+                }
+            }
+            for(int ii = 0; ii < 9; ii++) {
+                if(b[ii] == 0) return 0;
+            }
+            for(int ii = 0; ii < 3; ii++) {
+                if(sr[ii] != sr[0]) return 0;
+                if(sc[ii] != sr[0]) return 0;
+            }
+            if(d[0] != sr[0] || d[1] != sr[0]) return 0;
+            return 1;
+        };
+        int ans = 0;
+        for(int i = 0; i+2 < n; i++) {
+            for(int j = 0; j+2 < m; j++) {
+                ans += check(i, j);
+            }
+        }
+        return ans;
+    }
+};
