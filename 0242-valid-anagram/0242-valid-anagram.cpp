51133fdaef6422ffa121fffc4974d5cd235d0119
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-08-10 10:02:25 +0700

    Time: 0 ms (100%), Space: 8.7 MB (61.81%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         return s == t;
