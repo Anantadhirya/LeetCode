807b147aee17d6f5a7a512ff495c327fa3745af4
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-08-11 10:33:51 +0700

    Time: 296 ms (98.68%), Space: 168.4 MB (92.98%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -4,6 +4,7 @@ public:
     queue<pair<int, int>> q;
     int n, i = 0, cnt = 0;
     LRUCache(int capacity) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         n = capacity;
     }
     
