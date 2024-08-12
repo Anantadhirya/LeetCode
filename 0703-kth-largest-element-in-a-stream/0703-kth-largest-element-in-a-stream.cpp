07659544416476591797fe6fb785d138ba648541
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-08-12 07:57:33 +0700

    Time: 19 ms (95.63%), Space: 24.7 MB (39.1%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -3,6 +3,7 @@ public:
     int n;
     priority_queue<int> pq;
     KthLargest(int k, vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         n = k;
         for(const auto &i: nums) {
             pq.push(-i);
