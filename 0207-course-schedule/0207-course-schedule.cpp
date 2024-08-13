a8663e7c751b8987a8f66d251f983a7a50dda343
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-08-13 11:00:24 +0700

    Time: 7 ms (98.87%), Space: 17.1 MB (72.63%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = numCourses;
         vector<int> cnt(n, 0);
         vector<vector<int>> adj(n);
