5962e8dfb8f5489ce789a68d05d749a9320b6cdb
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-08-18 09:00:58 +0700

    Time: 91 ms (14.43%), Space: 34.4 MB (10.55%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        priority_queue<long long> pq;
+        unordered_map<long long, bool> vis;
+        pq.push(-1);
+        for(int cnt = 1; cnt < n; cnt++) {
+            while(vis[pq.top()]) pq.pop();
+            vis[pq.top()] = 1;
+            pq.push(pq.top()*2);
+            pq.push(pq.top()*3);
+            pq.push(pq.top()*5);
+            pq.pop();
+        }
+        while(vis[pq.top()]) pq.pop();
+        vis[pq.top()] = 1;
+        return -pq.top();
+    }
+};
