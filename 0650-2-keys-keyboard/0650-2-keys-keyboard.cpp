edc40325ee19879abb0c0c03aaf17ae01c1ec4c8
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-08-19 18:33:21 +0700

    Time: 22 ms (23.86%), Space: 8.3 MB (30.87%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minSteps(int n) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         vector<int> dp(n+1, INT_MAX);
         dp[1] = 0;
         for(int i = 2; i <= n; i++) {
