35454faaa4b52ecc889cd3f6377675e148795995
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-08-28 07:50:27 +0700

    Time: 408 ms (22.51%), Space: 112.1 MB (29.95%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+    void dfs(int i, int j, int &n, int &m, vector<vector<int>> &grid1, vector<vector<int>> &grid2, vector<vector<bool>> &vis, bool &valid) {
+        if(vis[i][j]) return;
+        if(!grid1[i][j]) valid = 0;
+        vis[i][j] = 1;
+        for(int d = 0, ii, jj; d < 4; d++) {
+            ii = i + dx[d];
+            jj = j + dy[d];
+            if(0 <= ii && ii < n && 0 <= jj && jj < m && grid2[ii][jj] && !vis[ii][jj]) {
+                dfs(ii, jj, n, m, grid1, grid2, vis, valid);
+            }
+        }
+    }
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        int n = grid2.size(), m = grid2[0].size();
+        vector<vector<bool>> vis(n, vector<bool>(m, 0));
+        bool valid;
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid2[i][j] && !vis[i][j]) {
+                    valid = 1;
+                    dfs(i, j, n, m, grid1, grid2, vis, valid);
+                    ans += valid;
+                }
+            }
+        }
+        return ans;
+    }
+};
