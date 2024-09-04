37b46b95e9b5dd2e634edfbc5b2d1fd6ad009818
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-09-04 07:36:02 +0700

    Time: 104 ms (37.29%), Space: 50.4 MB (22.38%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         map<pair<int, int>, bool> m;
         int dx[] = {0, 1, 0, -1};
         int dy[] = {1, 0, -1, 0};
