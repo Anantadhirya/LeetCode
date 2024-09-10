95d2dc7612ade0624275a717b33e4715331259b2
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-09-10 08:24:04 +0700

    Time: 36 ms (73.56%), Space: 35.5 MB (82.75%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
@@ -17,6 +17,7 @@ public:
         solve(head->next->next);
     }
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         solve(head);
         return head;
     }
