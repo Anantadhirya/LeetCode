48fa801f2b7fae8632cb74b8179408e3f00eeae7
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-09-11 16:11:29 +0700

    Time: 0 ms (100%), Space: 7.6 MB (44.87%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        return __builtin_popcount(start^goal);
+        ios_base::sync_with_stdio(false); cin.tie(0);
+        int x = start ^ goal;
+        int ans = 0;
+        while(x) {
+            ans += (x&1);
+            x /= 2;
+        }
+        return ans;
     }
 };
