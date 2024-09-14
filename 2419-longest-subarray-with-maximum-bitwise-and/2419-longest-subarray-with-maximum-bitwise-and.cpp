5abb4ae1ddfefa4f55c7dd60b112acc23b93ebcd
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-09-14 14:33:08 +0700

    Time: 56 ms (93.64%), Space: 85.6 MB (11.62%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int lst = -1, cnt = 0;
         pair<int, int> ans = {0, 0};
         for(const int &i: nums) {
