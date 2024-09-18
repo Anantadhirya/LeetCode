1fcc1b77e49b2ed42bf893a9c90dd796d07a9fc5
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-09-18 07:23:07 +0700

    Time: 5 ms (70.7%), Space: 17.4 MB (36.02%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = nums.size();
         vector<string> v(n);
         for(int i = 0; i < n; i++) {
