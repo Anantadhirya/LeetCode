6936cacfed737893e1206c785aa2715b8ddbf674
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-09-20 09:16:50 +0700

    Time: 6 ms (68.64%), Space: 13.3 MB (6.1%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        const long long key = 461;
+        const long long MOD = 1e9 + 7;
+        int n = s.size();
+        vector<long long> p(n+1), pref(n+5), suff(n+5);
+        string ans;
+        p[0] = 1;
+        for(int i = 1; i <= n; i++) {
+            p[i] = (p[i-1] * key) % MOD;
+        }
+        for(int i = 1; i <= n; i++) {
+            pref[i] = (pref[i-1] * key % MOD + s[i-1]) % MOD;
+        }
+        for(int i = n; i >= 1; i--) {
+            suff[i] = (suff[i+1] * key % MOD + s[i-1]) % MOD;
+        }
+        for(int i = n; i >= 1; i--) {
+            if(pref[i] != (suff[1] - suff[i+1] * p[i] % MOD + MOD) % MOD) continue;
+            ans = s.substr(i);
+            reverse(ans.begin(), ans.end());
+            return ans + s;
+        }
+        ans = s;
+        reverse(ans.begin(), ans.end());
+        return ans + s;
+    }
+};
