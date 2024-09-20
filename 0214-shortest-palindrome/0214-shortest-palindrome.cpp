078c5a1038b6397e9bddb4f45c5cac30da32e836
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-09-20 09:17:07 +0700

    Time: 2 ms (96.55%), Space: 13.6 MB (5.53%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         const long long key = 461;
         const long long MOD = 1e9 + 7;
         int n = s.size();
