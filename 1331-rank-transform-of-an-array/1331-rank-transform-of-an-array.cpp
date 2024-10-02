ffcad24fa6f7509ac8aa4f8efb28dd8464232b42
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-10-02 07:17:12 +0700

    Time: 38 ms (97.04%), Space: 36.7 MB (97.37%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        ios_base::sync_with_stdio(false); cin.tie(0);
-        map<int, int> rank;
-        for(const auto &i: arr) {
-            rank[i] = 0;
+        int n = arr.size();
+        if(n == 0) return arr;
+        vector<pair<int, int>> v(n);
+        for(int i = 0; i < n; i++) {
+            v[i] = {arr[i], i};
         }
-        int cnt = 0;
-        for(const auto &[i, _]: rank) {
-            rank[i] = ++cnt;
+        sort(v.begin(), v.end());
+        int lst = v[0].first, tmp;
+        v[0].first = 1;
+        for(int i = 1; i < n; i++) {
+            tmp = v[i].first;
+            v[i].first = v[i-1].first + (v[i].first != lst);
+            lst = tmp;
         }
-        for(auto &i: arr) {
-            i = rank[i];
+        for(const auto &[val, idx]: v) {
+            arr[idx] = val;
         }
         return arr;
     }
