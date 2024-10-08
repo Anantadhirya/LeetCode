3a7f4ee34851202dc151b092917444e7dd1e76aa
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-10-08 09:10:36 +0700

    Time: 45 ms (99.4%), Space: 32.6 MB (91.34%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int minSwaps(string s) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int cnt = 0, ans = 0;
-        for(auto &i: s) {
+        for(const auto &i: s) {
             if(i == '[') cnt++;
             else cnt--;
             if(cnt < 0) {
