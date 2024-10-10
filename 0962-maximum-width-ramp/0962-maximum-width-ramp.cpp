84a60ee33710a4535a3b00c1106242af653b004e
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-10-10 08:21:34 +0700

    Time: 55 ms (41.1%), Space: 54.6 MB (5.01%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.cpp
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         vector<pair<int, int>> v;
         int ans = 0, mn;
         int n = nums.size();
