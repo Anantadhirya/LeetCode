2d0b98d3c115feb49be03675feb68ff153aad03f
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-10-11 07:17:34 +0700

    Time: 142 ms (68.39%), Space: 56.9 MB (91.75%) - LeetHub

---
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        int n = times.size();
+        priority_queue<pair<int, int>> used;
+        priority_queue<int> pq;
+        int counter = 0, chair;
+        vector<array<int, 3>> v(n);
+        for(int i = 0; i < n; i++) {
+            v[i] = {times[i][0], times[i][1], i};
+        }
+        sort(v.begin(), v.end());
+        for(int i = 0; i < n; i++) {
+            while(!used.empty() && -used.top().first <= v[i][0]) {
+                pq.push(-used.top().second);
+                used.pop();
+            }
+            if(pq.empty()) pq.push(counter--);
+            chair = -pq.top();
+            if(v[i][2] == targetFriend) return chair;
+            pq.pop();
+            used.push({-v[i][1], chair});
+        }
+        return -1;
+    }
+};
