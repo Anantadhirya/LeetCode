4f999dd7dd637f119a4597478b643880f29ca176
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-10-12 08:50:33 +0700

    Time: 214 ms (96.62%), Space: 96.6 MB (70.27%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -2,13 +2,18 @@ class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
         ios_base::sync_with_stdio(false); cin.tie(0);
-        sort(intervals.begin(), intervals.end());
-        priority_queue<int> pq;
-        int ans = 0;
+        vector<int> v;
         for(const auto &i: intervals) {
-            while(!pq.empty() && -pq.top() < i[0]) pq.pop();
-            pq.push(-i[1]);
-            ans = max(ans, (int)pq.size());
+            v.push_back(2*i[1]+2);
+            v.push_back(2*i[0]+1);
+        }
+        sort(v.begin(), v.end());
+        int ans = 0, tmp = 0;
+        for(const auto &i: v) {
+            cout << i << "\n";
+            if(i&1) tmp++;
+            else tmp--;
+            ans = max(ans, tmp);
         }
         return ans;
     }
