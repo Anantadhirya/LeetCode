572029808c5eeb80316028bc3c0f537ee910f1c4
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-10-13 09:35:03 +0700

    Time: 33 ms (96.05%), Space: 25.9 MB (16.03%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int k = nums.size();
         set<pair<int, int>> s;
         pair<int, int> ans = {INT_MAX, 0};
