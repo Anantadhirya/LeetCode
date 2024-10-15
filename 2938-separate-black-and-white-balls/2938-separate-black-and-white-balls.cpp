22be7d7cc24f7ca7efdd48a717696979d601f2b7
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-10-15 08:30:27 +0700

    Time: 11 ms (100%), Space: 15.8 MB (14.11%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         long long ans = 0, cnt = 0;
         for(const auto &i: s) {
             if(i == '1') cnt++;
