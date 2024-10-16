e3d4750e04a40fedd92fdf96f028f5ed23c6afbd
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-10-16 08:05:11 +0700

    Time: 0 ms (100%), Space: 7.6 MB (90.57%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.cpp
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         string ans = "", tmp = "  ";
         char add = ' ';
         int mx;
