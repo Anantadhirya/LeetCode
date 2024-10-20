6cc21addca2bf6ddeb835eead477b6de47b8544d
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-10-20 10:58:54 +0700

    Time: 10 ms (44.83%), Space: 11.8 MB (35.86%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool parseBoolExpr(string expression) {
+        ios_base::sync_with_stdio(false); cin.tie(0);           
         vector<vector<char>> v(1);
         string s;
         char tmp;
