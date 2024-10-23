9d9d81761eded11d8daf05562f4194b6dc3c0e27
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-10-23 08:34:48 +0700

    Time: 15 ms (97.55%), Space: 330.7 MB (22.72%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -11,31 +11,31 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root, vector<int> &sm, int level) {
-        if(sm.size() == level) sm.push_back(0);
+    vector<int> sm;
+    void dfs(TreeNode* root, int level) {
         sm[level] += root->val;
-        if(root->left) dfs(root->left, sm, level+1);
-        if(root->right) dfs(root->right, sm, level+1);
+        if(root->left) dfs(root->left, level+1);
+        if(root->right) dfs(root->right, level+1);
     }
-    void dfs2(TreeNode* root, vector<int> &sm, int level) {
+    void dfs2(TreeNode* root, int level) {
         int child = 0;
         if(root->left) child += root->left->val;
         if(root->right) child += root->right->val;
         if(root->left) {
             root->left->val = sm[level+1] - child;
-            dfs2(root->left, sm, level+1);
+            dfs2(root->left, level+1);
         }
         if(root->right) {
             root->right->val = sm[level+1] - child;
-            dfs2(root->right, sm, level+1);
+            dfs2(root->right, level+1);
         }
     }
     TreeNode* replaceValueInTree(TreeNode* root) {
         ios_base::sync_with_stdio(false); cin.tie(0);
-        vector<int> sm(1e5, 0);
-        dfs(root, sm, 0);
+        sm.assign(1e5, 0);
+        dfs(root, 0);
         root->val = 0;
-        dfs2(root, sm, 0);
+        dfs2(root, 0);
         return root;
     }
 };
