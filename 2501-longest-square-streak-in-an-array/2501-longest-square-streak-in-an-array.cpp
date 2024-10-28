a24b689769eb6d1c5302a979838e2a6afc483419
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-10-28 21:21:53 +0700

    Time: 30 ms (93.33%), Space: 90 MB (86.17%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.cpp
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = nums.size();
         vector<int> v(n, 1);
         long long tmp;
