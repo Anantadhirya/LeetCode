8c83d265e0b6b5d6d41ad1843996ed2a0c649a0d
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-11-01 07:27:25 +0700

    Time: 24 ms (68.83%), Space: 42.6 MB (86.28%) - LeetHub

---
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string ans;
+        for(const auto &i: s) {
+            if(ans.size() >= 2 && ans.back() == i && ans[ans.size()-2] == i) continue;
+            ans.push_back(i);
+        }
+        return ans;
+    }
+};
