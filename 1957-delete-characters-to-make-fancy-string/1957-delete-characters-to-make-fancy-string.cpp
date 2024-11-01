3da6e1434571fc5b36518f47adcabec4cb3315dd
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-11-01 07:27:50 +0700

    Time: 31 ms (42.86%), Space: 42.9 MB (10.7%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string makeFancyString(string s) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         string ans;
         for(const auto &i: s) {
             if(ans.size() >= 2 && ans.back() == i && ans[ans.size()-2] == i) continue;
