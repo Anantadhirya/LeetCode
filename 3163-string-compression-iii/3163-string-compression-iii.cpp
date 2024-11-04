9b955119f330b0f2c375e70f988dffd67f741ff0
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-11-04 18:52:13 +0700

    Time: 58 ms (23.08%), Space: 42.1 MB (5.03%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.cpp
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string compressedString(string word) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         string ans = "";
         vector<pair<char, int>> v;
         for(auto &i: word) {
