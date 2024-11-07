5b74b7595e8be68724513eebfd11c787fdafbc24
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-11-07 07:39:53 +0700

    Time: 12 ms (94.78%), Space: 60.2 MB (74.4%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     int largestCombination(vector<int>& candidates) {
         ios_base::sync_with_stdio(false); cin.tie(0);
-        int mx = 0;
         int tmp, ans = 0;
-        for(const auto &i: candidates) {
-            mx = max(mx, i);
-        }
+        int mx = *max_element(candidates.begin(), candidates.end());
         for(; mx > 0; mx /= 2) {
             tmp = 0;
             for(int &i: candidates) {
