546664d50688a9b3a024e14183b6f891f8efea1b
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-11-13 08:51:59 +0700

    Time: 27 ms (94.05%), Space: 61 MB (5.96%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = nums.size();
         long long ans = 0;
         sort(nums.begin(), nums.end());
