1d71c96421b1d92d6d6784c61a9fae3bada98e73
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-11-15 12:37:49 +0700

    Time: 0 ms (100%), Space: 69.3 MB (98.14%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = arr.size();
-        int l = 0, r = n-1, ans;
+        int l = 0, r = n-1, ans, tmp;
         while(l+1 < n && arr[l] <= arr[l+1]) l++;
         while(r-1 >= 0 && arr[r-1] <= arr[r]) r--;
         ans = n-1-l;
         if(l == n-1) return 0;
-        for(int tmp; r < n; r++) {
+        for(; r < n; r++) {
             tmp = upper_bound(arr.begin(), arr.begin() + l + 1, arr[r]) - arr.begin();
             ans = min(ans, max(0, r - tmp));
         }
