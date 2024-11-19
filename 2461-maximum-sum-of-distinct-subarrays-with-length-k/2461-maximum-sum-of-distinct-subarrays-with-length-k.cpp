bc53362c361d467cab0856386170e36478711435
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-11-19 08:44:25 +0700

    Time: 15 ms (96.53%), Space: 109.3 MB (6.51%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         long long ans = 0, sm = 0;
         vector<int> cnt(1e5+1, 0);
         int n = nums.size(), unique = 0;
         for(int i = 0; i < k; i++) {
-            if(cnt[nums[i]] == 0) unique++;
-            cnt[nums[i]]++;
+            if(cnt[nums[i]]++ == 0) unique++;
             sm += nums[i];
         }
         if(unique == k) ans = max(ans, sm);
         for(int i = k; i < n; i++) {
-            if(cnt[nums[i]] == 0) unique++;
-            cnt[nums[i]]++;
+            if(++cnt[nums[i]] == 1) unique++;
             sm += nums[i];
-            cnt[nums[i-k]]--;
             sm -= nums[i-k];
-            if(cnt[nums[i-k]] == 0) unique--;
+            if(--cnt[nums[i-k]] == 0) unique--;
             if(unique == k) ans = max(ans, sm);
         }
         return ans;
