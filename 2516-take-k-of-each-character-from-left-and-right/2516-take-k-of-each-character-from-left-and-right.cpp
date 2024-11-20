cd7067bc2d1d903cb7bbb221a4dbe6cac68a152c
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-11-20 07:51:46 +0700

    Time: 0 ms (100%), Space: 12.1 MB (80.12%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
@@ -4,14 +4,13 @@ public:
         int n = s.size();
         int ans = n;
         vector<int> cnt(3, 0);
-        for(int i = 0; i < n; i++) {
-            cnt[s[i]-'a']++;
+        for(auto &i: s) {
+            cnt[i -= 'a']++;
         }
         if(cnt[0] < k || cnt[1] < k || cnt[2] < k) return -1;
-        for(int l = 0, r = 0, t; r < n; r++) {
-            t = s[r]-'a';
-            cnt[t]--;
-            while(cnt[t] < k) cnt[s[l++]-'a']++;
+        for(int l = 0, r = 0; r < n; r++) {
+            cnt[s[r]]--;
+            while(cnt[s[r]] < k) cnt[s[l++]]++;
             ans = min(ans, n - (r-l+1));
         }
         return ans;
