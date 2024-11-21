ec6f706ee14aac0a9de03b0bb567aa695908f7b5
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-11-21 07:40:33 +0700

    Time: 68 ms (37.11%), Space: 172.2 MB (62.09%) - LeetHub

---
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        vector<vector<int>> v(m, vector<int>(n, 0));
+        for(const auto &i: guards) {
+            v[i[0]][i[1]] = 1;
+        }
+        for(const auto &i: walls) {
+            v[i[0]][i[1]] = 2;
+        }
+        int active;
+        for(int i = 0; i < m; i++) {
+            active = 0;
+            for(int j = 0; j < n; j++) {
+                if(v[i][j] == 1) active = 1;
+                else if(v[i][j] == 2) active = 0;
+                else if(v[i][j] == 0 && active) v[i][j] = -1;
+            }
+            active = 0;
+            for(int j = n-1; j >= 0; j--) {
+                if(v[i][j] == 1) active = 1;
+                else if(v[i][j] == 2) active = 0;
+                else if(v[i][j] == 0 && active) v[i][j] = -1;
+            }
+        }
+        for(int j = 0; j < n; j++) {
+            active = 0;
+            for(int i = 0; i < m; i++) {
+                if(v[i][j] == 1) active = 1;
+                else if(v[i][j] == 2) active = 0;
+                else if(v[i][j] == 0 && active) v[i][j] = -1;
+            }
+            active = 0;
+            for(int i = m-1; i >= 0; i--) {
+                if(v[i][j] == 1) active = 1;
+                else if(v[i][j] == 2) active = 0;
+                else if(v[i][j] == 0 && active) v[i][j] = -1;
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                ans += (v[i][j] == 0);
+            }
+        }
+        return ans;
+    }
+};
