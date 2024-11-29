856fffcb5cefc35def7976ab18e9b21f49784626
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-11-29 08:21:05 +0700

    Time: 571 ms (33.83%), Space: 97.6 MB (40.97%) - LeetHub

---
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+    const int inf = INT_MAX;
+    int minimumTime(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, inf));
+        vector<vector<bool>> vis(n, vector<bool>(m, 0));
+        priority_queue<vector<int>> pq;
+        dist[0][0] = 0;
+        pq.push({-dist[0][0], 0, 0});
+        for(int i, j, ii, jj, d, tmp, adj; !pq.empty(); ) {
+            i = pq.top()[1];
+            j = pq.top()[2];
+            pq.pop();
+            if(vis[i][j]) continue;
+            vis[i][j] = 1;
+            adj = 0;
+            for(d = 0; d < 4; d++) {
+                ii = i + dx[d];
+                jj = j + dy[d];
+                if(ii < 0 || ii >= n || jj < 0 || jj >= m) continue;
+                if(grid[ii][jj] <= dist[i][j] + 1) {
+                    adj = 1;
+                    break;
+                }
+            }
+            for(d = 0; d < 4; d++) {
+                ii = i + dx[d];
+                jj = j + dy[d];
+                if(ii < 0 || ii >= n || jj < 0 || jj >= m) continue;
+                tmp = dist[i][j] + 1;
+                if(dist[i][j] + 1 < grid[ii][jj]) {
+                    if(!adj) continue;
+                    tmp = grid[ii][jj] + (dist[i][j] % 2 == grid[ii][jj] % 2);
+                }
+                if(tmp < dist[ii][jj]) {
+                    dist[ii][jj] = tmp;
+                    pq.push({-tmp, ii, jj});
+                }
+            }
+        }
+        return dist[n-1][m-1] == inf ? -1 : dist[n-1][m-1];
+    }
+};
