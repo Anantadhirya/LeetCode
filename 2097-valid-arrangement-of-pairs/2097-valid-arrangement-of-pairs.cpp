a8f1bcc6b6a8abcebc7a3fcfcfbae47fc12b859f
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-11-30 19:32:31 +0700

    Time: 284 ms (91.9%), Space: 243.7 MB (96.27%) - LeetHub

--- 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.cpp
+++ 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-    void dfs(int curr, unordered_map<int, vector<int>> &adj, vector<vector<int>> &ans) {
-        for(int to; !adj[curr].empty(); ) {
-            to = adj[curr].back();
-            adj[curr].pop_back();
-            dfs(to, adj, ans);
-            ans.push_back({to, curr});
-        }
-    }
     vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
         ios_base::sync_with_stdio(false); cin.tie(0);
         unordered_map<int, vector<int>> adj;
         unordered_map<int, int> cnt;
-        int start = pairs[0][0];
+        int start = pairs[0][0], curr, to;
         vector<vector<int>> ans;
+        stack<int> s;
         for(auto &i: pairs) {
             cnt[i[0]]++;
             cnt[i[1]]--;
@@ -22,7 +15,17 @@ public:
         for(auto &[i, c]: cnt) {
             if(c < 0) start = i;
         }
-        dfs(start, adj, ans);
+        s.push(start);
+        while(!s.empty()) {
+            curr = s.top();
+            if(!adj[curr].empty()) {
+                s.push(adj[curr].back());
+                adj[curr].pop_back();
+            } else {
+                s.pop();
+                if(!s.empty()) ans.push_back({curr, s.top()});
+            }
+        }
         return ans;
     }
 };
