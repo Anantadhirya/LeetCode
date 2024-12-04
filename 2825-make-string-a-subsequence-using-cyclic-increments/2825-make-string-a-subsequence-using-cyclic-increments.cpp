7799d9c3f77082c523657efc9f4ee07e7fb85d49
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-12-04 08:30:05 +0700

    Time: 3 ms (66.85%), Space: 16.8 MB (67.36%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool canMakeSubsequence(string str1, string str2) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = str1.size(), m = str2.size();
         for(int i = 0, j = 0; i < n; i++) {
             if(str1[i] == str2[j] || (str1[i] - 'a' + 1) % 26 + 'a' == str2[j]) {
