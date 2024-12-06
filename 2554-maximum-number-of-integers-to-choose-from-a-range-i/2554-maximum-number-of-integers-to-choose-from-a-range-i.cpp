444961447484ad9654a9b4d37fa3e7399b7b1cf2
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-12-06 09:09:09 +0700

    Time: 37 ms (93.13%), Space: 118 MB (78.9%) - LeetHub

--- 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int ans = 0, m = banned.size();
         sort(banned.begin(), banned.end());
         for(int i = 1, j = 0; i <= n; i++) {
