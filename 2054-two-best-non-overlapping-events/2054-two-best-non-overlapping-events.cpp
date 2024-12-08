7751ed22fad75240dbaf2672e1d3983f16117c7c
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-12-08 09:47:35 +0700

    Time: 55 ms (86.94%), Space: 126.2 MB (73.44%) - LeetHub

---
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+        priority_queue<pair<int, int>> pq;
+        int mx = 0, ans = 0;
+        sort(events.begin(), events.end());
+        for(const auto &i: events) {
+            while(!pq.empty() && -pq.top().first < i[0]) {
+                mx = max(mx, pq.top().second);
+                pq.pop();
+            }
+            pq.push({-i[1], i[2]});
+            ans = max(ans, i[2] + mx);
+        }
+        return ans;
+    }
+};
