be5b9f10ae544f5edde2cd023e160fe76cdd72e6
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-12-11 07:49:06 +0700

    Time: 50 ms (63.14%), Space: 104.6 MB (98.71%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = nums.size(), ans = 0;
         sort(nums.begin(), nums.end());
         for(int i = 0, j = 0; i < n; i++) {
