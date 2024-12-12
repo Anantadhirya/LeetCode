e189feb10c972a4ffd9e7c5c9576853d4a90d5ca
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-12-12 07:34:56 +0700

    Time: 0 ms (100%), Space: 13.9 MB (7.74%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         long long ans = 0;
         priority_queue<int> pq(gifts.begin(), gifts.end());
         while(k--) {
