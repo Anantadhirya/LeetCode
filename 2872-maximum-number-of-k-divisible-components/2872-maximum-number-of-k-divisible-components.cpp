a1790f2ced16184241831c42bb5463bae6193518
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-12-21 08:53:12 +0700

    Time: 78 ms (99.37%), Space: 173.2 MB (86.08%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    void dfs(int curr, int from, vector<vector<int>> &adj, vector<long long> &v, int &k, int &ans) {
+    long long dfs(int curr, int from, vector<vector<int>> &adj, vector<int> &values, int &k, int &ans) {
+        long long sm = values[curr];
         for(const int &to: adj[curr]) {
             if(to == from) continue;
-            dfs(to, curr, adj, v, k, ans);
-            v[curr] += v[to];
+            sm += dfs(to, curr, adj, values, k, ans);
         }
-        ans += (v[curr] % k == 0);
+        ans += (sm % k == 0);
+        return sm;
     }
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
         vector<vector<int>> adj(n);
-        vector<long long> v(values.begin(), values.end());
         int ans = 0;
         for(const auto &i: edges) {
             adj[i[0]].push_back(i[1]);
             adj[i[1]].push_back(i[0]);
         }
-        dfs(0, -1, adj, v, k, ans);
+        dfs(0, -1, adj, values, k, ans);
         return ans;
     }
 };
