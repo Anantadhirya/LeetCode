a2c551a134066cd4e9057ddfbfb797260cce5f89
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2024-12-26 09:14:59 +0700

    Time: 15 ms (59.97%), Space: 13.9 MB (58.51%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        vector<int> ans(2005, 0), ans2(2005, 0);
-        ans[1000] = 1;
+        ios_base::sync_with_stdio(false); cin.tie(0);
+        int sm = 0;
+        for(const auto &i: nums) {
+            sm += i;
+        }
+        vector<int> ans(2*sm+1, 0), ans2(2*sm+1, 0);
+        ans[sm] = 1;
         for(auto &i: nums) {
             swap(ans, ans2);
-            for(int j = 0; j <= 2000; j++) {
+            for(int j = 0; j <= 2*sm; j++) {
                 ans[j] = 0;
                 if(j - i >= 0) ans[j] += ans2[j-i];
-                if(j + i <= 2000) ans[j] += ans2[j+i];
+                if(j + i <= 2*sm) ans[j] += ans2[j+i];
             }
         }
-        return ans[target+1000];
+        return -sm <= target && target <= sm ? ans[target+sm] : 0;
     }
 };
