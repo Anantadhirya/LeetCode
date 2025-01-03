d1430a37e974ead26341410375cf002485907d90
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-01-03 17:09:14 +0700

    Time: 0 ms (100%), Space: 89.5 MB (44.2%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
-        long long sum = 0, pref = 0;
-        int ans = 0;
+        ios_base::sync_with_stdio(false); cin.tie(0);
+        long long sum = 0;
         for(const auto &i: nums) {
             sum += i;
         }
+        long long pref = 0;
+        int ans = 0;
         nums.pop_back();
         for(const auto &i: nums) {
             pref += i;
