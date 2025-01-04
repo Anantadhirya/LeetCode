f053288d716b3afb634784aa10fa30ad1e16aa00
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-01-04 09:07:21 +0700

    Time: 43 ms (96.01%), Space: 14 MB (96.63%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = s.size();
         int ans = 0, cnt;
         bitset<26> b;
