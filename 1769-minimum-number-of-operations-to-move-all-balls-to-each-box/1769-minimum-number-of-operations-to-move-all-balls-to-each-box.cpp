465fbbebd327d77f0ba0faa01f399d016de1d843
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-01-06 08:33:55 +0700

    Time: 0 ms (100%), Space: 12.2 MB (50.97%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = boxes.size(), l = 0, r = 0;
         vector<int> ans(n, 0);
         for(int i = 0; i < n; i++) {
