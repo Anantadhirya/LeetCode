7efa97a3e2ea4a95d63a58d06c6464fe9c12866e
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-01-13 09:30:10 +0700

    Time: 0 ms (100%), Space: 32.2 MB (58.06%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minimumLength(string s) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         vector<int> cnt(26, 0);
         int ans = 0;
         for(const auto &c: s) {
