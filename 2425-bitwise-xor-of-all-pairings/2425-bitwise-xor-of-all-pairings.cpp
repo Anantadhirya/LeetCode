39014be7c4a3420562d7404a2b94686ae5029496
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-01-16 09:32:55 +0700

    Time: 0 ms (100%), Space: 64.4 MB (36.18%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.cpp
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
-        int cnt1, cnt2, cnt, ans = 0, n = nums1.size(), m = nums2.size();
-        for(int i = 0; i < 30; i++) {
-            cnt1 = cnt2 = 0;
-            for(const auto &j: nums1) {
-                cnt1 += ((j>>i)&1);
-            }
-            for(const auto &j: nums2) {
-                cnt2 += ((j>>i)&1);
-            }
-            cnt = (cnt1 * (m - cnt2)) + (cnt2 * (n - cnt1));
-            ans += (cnt&1) * (1<<i);
+        int xor1 = 0, xor2 = 0, ans = 0, n = nums1.size(), m = nums2.size();
+        for(const int &i: nums1) {
+            xor1 ^= i;
         }
+        for(const int &i: nums2) {
+            xor2 ^= i;
+        }
+        if(m&1) ans ^= xor1;
+        if(n&1) ans ^= xor2;
         return ans;
     }
 };
