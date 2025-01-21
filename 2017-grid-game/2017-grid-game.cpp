7473c7e0b3867982a6e93615b55a0c1dcebdcb21
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-01-21 08:23:26 +0700

    Time: 0 ms (100%), Space: 66.7 MB (61.54%) - LeetHub

--- 2017-grid-game/2017-grid-game.cpp
+++ 2017-grid-game/2017-grid-game.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
-        int n = grid[0].size();
-        if(n == 1) return 0;
-        vector<vector<long long>> pref(2, vector<long long>(n+2, 0));
-        for(int i = 0; i < 2; i++) {
-            for(int j = 1; j <= n; j++) {
-                pref[i][j] = pref[i][j-1] + grid[i][j-1];
-            }
-        }
+        long long top = 0, bot = 0;
         long long ans = LLONG_MAX;
-        for(int i = 1; i <= n; i++) {
-            ans = min(ans, max(pref[0][n] - pref[0][i], pref[1][i-1]));
+        int n = grid[0].size();
+        for(const int &i: grid[0]) {
+            top += i;
+        }
+        for(int i = 0; i < n; i++) {
+            top -= grid[0][i];
+            ans = min(ans, max(top, bot));
+            bot += grid[1][i];
         }
         return ans;
     }
