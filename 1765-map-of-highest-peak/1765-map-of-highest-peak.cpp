d1b114eb9b6857d919266de75ae70a23285b24b9
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-01-22 09:09:52 +0700

    Time: 75 ms (74.51%), Space: 111.7 MB (91.63%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.cpp
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.cpp
@@ -4,13 +4,14 @@ int dy[] = {0, 0, 1, -1};
 class Solution {
 public:
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = isWater.size(), m = isWater[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, 0));
         vector<vector<bool>> vis(n, vector<bool>(m, 0));
         queue<pair<int, int>> q;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(isWater[i][j] == 1) {
+                    isWater[i][j] = 0;
                     q.push({i, j});
                     vis[i][j] = 1;
                 }
@@ -24,12 +25,12 @@ public:
                 ii = i + dx[d];
                 jj = j + dy[d];
                 if(0 <= ii && ii < n && 0 <= jj && jj < m && !vis[ii][jj]) {
-                    ans[ii][jj] = ans[i][j] + 1;
+                    isWater[ii][jj] = isWater[i][j] + 1;
                     q.push({ii, jj});
                     vis[ii][jj] = 1;
                 }
             }
         }
-        return ans;
+        return isWater;
     }
 };
