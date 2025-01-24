92b0829701eb8da9f49e41a59fe8b9fc92294497
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-01-24 09:18:39 +0700

    Time: 58 ms (21.37%), Space: 66.1 MB (42.62%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        ios_base::sync_with_stdio(false); cin.tie(0);
         int n = graph.size();
         vector<int> cnt(n, 0);
         vector<vector<int>> adj(n);
