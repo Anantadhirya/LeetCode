3a26c498bbcf8c1e0ea00b8c9890e941566b4760
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-02-03 09:30:33 +0700

    Time: 388 ms (16.93%), Space: 140 MB (5.12%) - LeetHub

---
+++ 2512-reward-top-k-students/2512-reward-top-k-students.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> split(string s) {
+        vector<string> ret(1);
+        for(const auto &c: s) {
+            if(c == ' ') ret.push_back("");
+            else ret.back() += c;
+        }
+        return ret;
+    }
+    vector<int> topStudents(vector<string>& positive_feedback, vector<string>& negative_feedback, vector<string>& report, vector<int>& student_id, int k) {
+        map<string, int> m;
+        map<int, int> score;
+        vector<int> ans(k);
+        vector<pair<int, int>> score_pair;
+        int n = report.size();
+        for(const string &s: positive_feedback) {
+            m[s] = 3;
+        }
+        for(const string &s: negative_feedback) {
+            m[s] = -1;
+        }
+        for(int i = 0; i < n; i++) {
+            for(const string &word: split(report[i])) {
+                score[student_id[i]] += m[word];
+            }
+        }
+        for(const auto &[id, x]: score) {
+            score_pair.push_back({-x, id});
+        }
+        sort(score_pair.begin(), score_pair.end());
+        for(int i = 0; i < k; i++) {
+            ans[i] = score_pair[i].second;
+        }
+        return ans;
+    }
+};
