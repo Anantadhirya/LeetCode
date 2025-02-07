f41756675ae7a5a2c352990dee84ce320e95b29e
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-02-07 09:30:10 +0700

    Time: 116 ms (55.31%), Space: 154.5 MB (94.86%) - LeetHub

--- 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -1,3 +1,5 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return NULL; }();
+
 class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
