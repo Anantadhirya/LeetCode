f4e38d642f6412d437dec01ccd9d82af4f4926d5
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-02-12 07:39:09 +0700

    Time: 7 ms (94.6%), Space: 63.8 MB (75.41%) - LeetHub

---
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumSum(vector<int>& nums) {
+        vector<int> mx(90, -1);
+        int ans = -1, sm;
+        for(const int &i: nums) {
+            sm = 0;
+            for(int j = i; j > 0; j /= 10) {
+                sm += j%10;
+            }
+            if(mx[sm] != -1) ans = max(ans, mx[sm] + i);
+            mx[sm] = max(mx[sm], i);
+        }
+        return ans;
+    }
+};
