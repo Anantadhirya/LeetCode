20bd9e18fbbaf1aecee9052c3b76697ed7feeea4
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-02-25 08:10:02 +0700

    Time: 0 ms (100%), Space: 112.4 MB (25.5%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,16 +1,22 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return NULL; }();
+
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
         const int MOD = 1e9 + 7;
         int ans = 0, sm = 0;
-        int pref[2];
-        pref[0] = 1;
-        pref[1] = 0;
+        int odd = 0, even = 1;
         for(const auto &i: arr) {
-            (sm += i) &= 1;
-            (ans += pref[1-sm]) %= MOD;
-            pref[sm]++;
+            sm += i;
+            if(sm&1) {
+                odd++;
+                ans += even;
+            } else {
+                even++;
+                ans += odd;
+            }
+            if(ans > MOD) ans -= MOD;
         }
-        return ans;
+        return ans % MOD;
     }
 };
