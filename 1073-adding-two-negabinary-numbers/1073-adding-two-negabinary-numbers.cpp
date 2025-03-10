dbb18b116dd95349301df8baaeab9bfa9377426b
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-03-10 13:05:27 +0700

    Time: 0 ms (100%), Space: 23.5 MB (36.19%) - LeetHub

---
+++ 1073-adding-two-negabinary-numbers/1073-adding-two-negabinary-numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> addNegabinary(vector<int>& arr1, vector<int>& arr2) {
+        int n = arr1.size(), m = arr2.size(), curr = 0;
+        vector<int> ans;
+        while(n > 0 || m > 0 || curr != 0) {
+            curr += (n > 0 ? arr1[--n] : 0) + (m > 0 ? arr2[--m] : 0);
+            if(curr == -1) ans.push_back(1), curr = 1;
+            else if(curr == 0) ans.push_back(0);
+            else if(curr == 1) ans.push_back(1), curr = 0;
+            else if(curr == 2) ans.push_back(0), curr = -1;
+            else if(curr == 3) ans.push_back(1), curr = -1;
+        }
+        while(ans.back() == 0 && ans.size() > 1) ans.pop_back();
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
