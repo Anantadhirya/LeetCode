e846b8c7e991f799af411cd58977e4dc98250673
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-03-10 12:02:46 +0700

    Time: 0 ms (100%), Space: 8 MB (68.42%) - LeetHub

---
+++ 1017-convert-to-base-2/1017-convert-to-base-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string baseNeg2(int n) {
+        if(n == 0) return "0";
+        string ans;
+        while(n != 0) {
+            if(abs(n) % 2 == 1) {
+                ans += '1';
+                n--;
+            } else {
+                ans += '0';
+            }
+            n = -n/2;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
