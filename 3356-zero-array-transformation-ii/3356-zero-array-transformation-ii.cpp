060d651e0e359a4fb49e15195b81b6e7f12720a2
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-03-13 08:40:27 +0700

    Time: 9 ms (79.62%), Space: 323 MB (85.62%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.cpp
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size(), m = queries.size(), ans = -1, sm;
-        vector<vector<pair<int, int>>> add(n+1);
-        for(int i = 0; i < m; i++) {
-            add[queries[i][0]].push_back({i, queries[i][2]});
-            add[queries[i][1]+1].push_back({i, -queries[i][2]});
-        }
+        int n = nums.size(), m = queries.size(), ans = 0;
+        int l, r, val;
+        vector<int> arr(n+1);
         for(int i = 0; i < n; i++) {
-            for(int j = 1; j < add[i].size(); j++) {
-                add[i][j].second += add[i][j-1].second;
+            while(arr[i] < nums[i]) {
+                if(ans == m) return -1;
+                l = queries[ans][0];
+                r = queries[ans][1];
+                val = queries[ans++][2];
+                
+                if(r < i) continue;
+                arr[max(l, i)] += val;
+                arr[r+1] -= val;
             }
-        }
-        for(int l = 0, r = m, mid, valid; l <= r; ) {
-            mid = (l + r)/2;
-            sm = 0;
-            valid = 1;
-            for(int i = 0; i < n; i++) {
-                auto it = lower_bound(add[i].begin(), add[i].end(), make_pair(mid, INT_MIN));
-                if(it != add[i].begin()) sm += (--it)->second;
-                if(sm < nums[i]) {
-                    valid = 0;
-                    break;
-                }
-            }
-            if(valid) {
-                ans = mid;
-                r = mid - 1;
-            } else l = mid + 1;
+            arr[i+1] += arr[i];
         }
         return ans;
     }
