67f4fa450d9f7a37fee8c5a17f72037af2bed4a9
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-03-13 08:41:03 +0700

    Time: 9 ms (79.62%), Space: 322.8 MB (96.06%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.cpp
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.cpp
@@ -1,3 +1,5 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); return nullptr; }();
+
 class Solution {
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
