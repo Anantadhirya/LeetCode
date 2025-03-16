03a14337ceb3269f04fa34ea65bc7cc2c4cc0d4d
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-03-16 11:51:01 +0700

    Time: 2 ms (97.18%), Space: 60.3 MB (5.65%) - LeetHub

--- 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.cpp
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.cpp
@@ -3,12 +3,16 @@ const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nu
 class Solution {
 public:
     long long repairCars(vector<int>& ranks, int cars) {
+        unordered_map<int, int> m;
         long long ans = -1, l = 0, r = (long long)*min_element(ranks.begin(), ranks.end()) * cars * cars, mid, cnt;
+        for(const int &i: ranks) {
+            m[i]++;
+        }
         while(l <= r) {
             mid = (l+r)/2;
             cnt = 0;
-            for(const int &i: ranks) {
-                cnt += floor(sqrt((double)mid/i));
+            for(const auto &[r, c]: m) {
+                cnt += (long long)c * floor(sqrt((double)mid/r));
             }
             if(cnt >= cars) {
                 ans = mid;
