b81737b2b8ede486b20788a3e687eb310f3c8d47
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-03-16 11:47:22 +0700

    Time: 63 ms (31.07%), Space: 59.3 MB (74.95%) - LeetHub

---
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long repairCars(vector<int>& ranks, int cars) {
+        long long ans = -1, l = 0, r = (long long)*min_element(ranks.begin(), ranks.end()) * cars * cars, mid, cnt;
+        while(l <= r) {
+            mid = (l+r)/2;
+            cnt = 0;
+            for(const int &i: ranks) {
+                cnt += floor(sqrt((double)mid/i));
+            }
+            if(cnt >= cars) {
+                ans = mid;
+                r = mid - 1;
+            } else l = mid + 1;
+        }
+        return ans;
+    }
+};
