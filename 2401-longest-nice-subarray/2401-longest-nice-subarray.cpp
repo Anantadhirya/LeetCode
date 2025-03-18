ad463101e42fd8ac80820c457ff6a96b930fa7dc
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-03-18 14:00:03 +0700

    Time: 0 ms (100%), Space: 60.9 MB (87.7%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        const int MX = 32;
-        vector<int> cnt(32, 0);
-        int ans = 0, n = nums.size(), more = 0;
+        int mask = 0;
+        int ans = 0, n = nums.size();
         for(int i = 0, j = 0; i < n; i++) {
-            for(int k = 0; k < MX; k++) {
-                if((nums[i]>>k)&1) {
-                    more += (cnt[k] == 1);
-                    cnt[k]++;
-                }
+            while(mask & nums[i]) {
+                mask ^= nums[j++];
             }
-            while(more) {
-                for(int k = 0; k < MX; k++) {
-                    if((nums[j]>>k)&1) {
-                        cnt[k]--;
-                        more -= (cnt[k] == 1);
-                    }
-                }
-                j++;
-            }
+            mask ^= nums[i];
             ans = max(ans, i-j+1);
         }
         return ans;
