3ebf4e1f116951ff6a887bb2e7f06ba05889500b
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-03-19 07:28:33 +0700

    Time: 11 ms (31.84%), Space: 140.2 MB (15.99%) - LeetHub

--- 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,15 +1,17 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
         int n = nums.size(), ans = 0;
-        for(int i = n-1; i-2 >= 0; i--) {
+        for(int i = n-1; i >= 0; i--) {
             if(nums[i] == 0) {
                 ans++;
-                nums[i-1] ^= 1;
-                nums[i-2] ^= 1;
+                if(i-2 < 0) return -1;
+                nums[i-1] = 1 - nums[i-1];
+                nums[i-2] = 1 - nums[i-2];
             }
         }
-        if(nums[0] == 0 || nums[1] == 0) return -1;
         return ans;
     }
 };
