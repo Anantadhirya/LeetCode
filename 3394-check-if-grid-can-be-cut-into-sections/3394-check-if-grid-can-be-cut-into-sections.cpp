3d16ef82f97caf71eaa21977582f33cd8c0898ec
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-03-25 07:58:44 +0700

    Time: 84 ms (84.25%), Space: 206.9 MB (71.12%) - LeetHub

---
+++ 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkValidCuts(int k, vector<vector<int>>& rectangles) {
+        vector<int> start[2], end[2];
+        int n = rectangles.size(), cnt, ans;
+        for(const auto &i: rectangles) {
+            start[0].push_back(i[0]);
+            start[1].push_back(i[1]);
+            end[0].push_back(i[2]);
+            end[1].push_back(i[3]);
+        }
+        for(int p = 0; p < 2; p++) {
+            sort(start[p].begin(), start[p].end());
+            sort(end[p].begin(), end[p].end());
+            cnt = ans = 0;
+            for(int i = 0, j = 0; i < n; i++) {
+                cnt++;
+                if(i != 0 && start[p][i] == start[p][i-1]) continue;
+                while(j < n && end[p][j] <= start[p][i]) cnt--, j++;
+                if(cnt == 1) ans++;
+                if(ans >= 3) return 1;
+            }
+        }
+        return 0;
+    }
+};
