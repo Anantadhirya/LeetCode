5c9bae82ae0bd4c2df067d1596c026b58488cca3
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-03-27 08:36:58 +0700

    Time: 34 ms (55.33%), Space: 98.7 MB (47.95%) - LeetHub

---
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        unordered_map<int, int> cnt;
+        int x = 0, mx = 0, n = nums.size();
+        for(const int &i: nums) {
+            cnt[i]++;
+        }
+        for(const auto &[i, c]: cnt) {
+            if(c > mx) mx = c, x = i;
+        }
+        for(int i = 0, l = 0, r; i < n - 1; i++) {
+            l += (nums[i] == x);
+            r = mx - l;
+            if(l*2 > i+1 && r*2 > n-1-i) return i;
+        }
+        return -1;
+    }
+};
