4603b1c114be90cd13d90755e82bafd72e5e553e
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-04-16 10:44:18 +0700

    Time: 67 ms (95.73%), Space: 78.6 MB (93.84%) - LeetHub

--- 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.cpp
+++ 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.cpp
@@ -1,15 +1,14 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 class Solution {
 public:
     long long countGood(vector<int>& nums, int k) {
-        int n = nums.size();
-        long long ans = 0, tmp = 0;
-        unordered_map<int, long long> cnt;
+        int n = nums.size(), tmp = 0;
+        long long ans = 0;
+        unordered_map<int, int> cnt;
         for(int l = 0, r = 0; r < n; r++) {
             tmp += cnt[nums[r]]++;
-            while(tmp >= k) {
-                tmp -= --cnt[nums[l]];
-                l++;
-            }
+            while(tmp >= k) tmp -= --cnt[nums[l++]];
             ans += l;
         }
         return ans;
