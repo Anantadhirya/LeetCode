24a12cc15c49ba2dcbe9ce9a3e77ae19aabc08f6
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-04-22 13:01:35 +0700

    Time: 8 ms (69.72%), Space: 9.4 MB (75.23%) - LeetHub

--- 2338-count-the-number-of-ideal-arrays/2338-count-the-number-of-ideal-arrays.cpp
+++ 2338-count-the-number-of-ideal-arrays/2338-count-the-number-of-ideal-arrays.cpp
@@ -1,12 +1,11 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 class Solution {
 public:
     int idealArrays(int n, int maxValue) {
-        const int MX = 1e4;
         const int MOD = 1e9 + 7;
         int k = ceil(1.0*log(maxValue)/log(2))+1;
-        bitset<MX+5> composit;
-        vector<int> primes;
-        vector<int> dp(k+1, 0);
+        vector<int> fac(maxValue+1), dp(k+1, 0);
         int ans = 0, tmp;
         dp[0] = 1;
         for(int i = 1; i <= n; i++) {
@@ -14,20 +13,19 @@ public:
                 (dp[j] += dp[j-1]) %= MOD; 
             }
         }
+        for(int i = 1; i <= maxValue; i++) fac[i] = i;
         for(int i = 2; i*i <= maxValue; i++) {
-            if(composit[i]) continue;
-            primes.push_back(i);
-            for(int j = i*i; j <= maxValue; j += i) composit[j] = 1;
+            if(fac[i] != i) continue;
+            for(int j = i*i; j <= maxValue; j += i) if(fac[j] == j) fac[j] = i;
         }
-        for(int i = 1, x, p; i <= maxValue; i++) {
+        for(int i = 1, x, p, pref; i <= maxValue; i++) {
             tmp = 1;
-            x = i;
-            for(int j = 0; j < primes.size() && primes[j] * primes[j] <= x; j++) {
-                p = 0;
-                while(x % primes[j] == 0) x /= primes[j], p++;
-                tmp = ((long long)tmp * dp[p]) % MOD;
+            pref = -1, p = 0;
+            for(x = i; x != 1; x /= fac[x]) {
+                if(fac[x] == pref) p++;
+                else tmp = ((long long)tmp * dp[p])%MOD, pref = fac[x], p = 1;
             }
-            if(x != 1) tmp = ((long long)tmp * dp[1]) % MOD;
+            tmp = ((long long)tmp * dp[p]) % MOD;
             (ans += tmp) %= MOD;
         }
         return ans;
