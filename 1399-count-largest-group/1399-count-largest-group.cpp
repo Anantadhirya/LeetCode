c785e2fc408d464321b0ceaed1877a3b03f05300
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-04-23 09:08:09 +0700

    Time: 4 ms (13.22%), Space: 8.3 MB (13.56%) - LeetHub

---
+++ 1399-count-largest-group/1399-count-largest-group.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        unordered_map<int, int> cnt;
+        for(int i = 1, j; i <= n; i++) {
+            j = 0;
+            for(int k = i; k > 0; k /= 10) j += k%10;
+            cnt[j]++;
+        }
+        int ans = 0, mx = 0;
+        for(const auto &[_, i]: cnt) {
+            if(i > mx) mx = i, ans = 1;
+            else if(i == mx) ans++;
+        }
+        return ans;
+    }
+};
