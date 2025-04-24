9e6f87d18a69de37a9f549d65f047fac6473000d
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-04-24 12:37:11 +0700

    Time: 3 ms (99.33%), Space: 48.1 MB (25.84%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
-        unordered_map<int, int> cnt;
-        unordered_set<int> s(nums.begin(), nums.end());
-        int n = nums.size(), missing = s.size(), ans = 0;
+        vector<int> cnt(2005, 0);
+        bitset<2005> b;
+        int n = nums.size(), missing = 0, ans = 0;
+        for(const int &i: nums) if(!b[i]) b[i] = 1, missing++;
         for(int l = 0, r = 0; r < n; r++) {
             if(cnt[nums[r]]++ == 0) missing--;
             while(l <= r && missing == 0) if(--cnt[nums[l++]] == 0) missing++;
