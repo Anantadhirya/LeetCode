60f44e6eb71caf5933053a51bee5d64f40b25874
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-04-25 11:15:47 +0700

    Time: 3 ms (100%), Space: 108.2 MB (98.91%) - LeetHub

--- 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
@@ -3,12 +3,14 @@ public:
     long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
         // pref - l == k mod modulo
         // pref - k == l mod modulo
-        unordered_map<int, int> m;
-        long long ans = 0, pref = 0;
+        long long ans = 0;
+        int pref = 0, l, n = min(modulo, (int)nums.size()+1);
+        vector<int> m(n, 0);
         m[0] = 1;
         for(const int &i: nums) {
             (pref += (i % modulo == k)) %= modulo;
-            ans += m[(pref-k+modulo) % modulo];
+            l = (pref-k+modulo) % modulo;
+            if(l < n) ans += m[l];
             m[pref]++;
         }
         return ans;
