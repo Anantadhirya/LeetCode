73d932c8152bb961015df4276af038bee5b037a1
Daniel Anantadhirya Adyawisesa Linan <78126769+Anantadhirya@users.noreply.github.com>
2025-05-10 20:17:00 +0700

    Time: 56 ms (98.98%), Space: 136.5 MB (5.08%) - LeetHub

--- 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -1,3 +1,5 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 class Solution {
 public:
     long long minSum(vector<int>& nums1, vector<int>& nums2) {
