f600adaa0dbbc9ef6dd79ba510e511dfff019fa3
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-05-14 09:37:15 +0700

    Time: 472 ms (8.33%), Space: 51.9 MB (81.25%) - LeetHub

---
+++ 3337-total-characters-in-string-after-transformations-ii/3337-total-characters-in-string-after-transformations-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    vector<vector<int>> mul(vector<vector<int>> &a, vector<vector<int>> &b) {
+        vector<vector<int>> ret(26, vector<int>(26, 0));
+        for(int i = 0; i < 26; i++) {
+            for(int j = 0; j < 26; j++) {
+                for(int k = 0; k < 26; k++) {
+                    (ret[i][j] += (long long)a[i][k] * b[k][j] % MOD) %= MOD; 
+                }
+            }
+        }
+        return ret;
+    }
+    int lengthAfterTransformations(string s, int t, vector<int>& nums) {
+        vector<int> cnt(26, 0);
+        vector<vector<int>> mat, tmp;
+        int ans = 0;
+        for(const auto &i: s) {
+            cnt[i-'a']++;
+        }
+        mat = tmp = vector<vector<int>>(26, vector<int>(26, 0));
+        for(int i = 0; i < 26; i++) {
+            mat[i][i] = 1;
+            for(int j = 1; j <= nums[i]; j++) {
+                tmp[(i+j)%26][i] = 1;
+            }
+        }
+        while(t) {
+            if(t&1) {
+                mat = mul(mat, tmp);
+            }
+            tmp = mul(tmp, tmp);
+            t >>= 1;
+        }
+        for(int i = 0; i < 26; i++) {
+            for(int j = 0; j < 26; j++) {
+                (ans += (long long)mat[i][j] * cnt[j] % MOD) %= MOD;
+            }
+        }
+        return ans;
+    }
+};
