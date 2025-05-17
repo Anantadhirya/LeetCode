1a07be5a95d4c99bf2bbf2537ae572d669467a36
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-05-17 07:55:51 +0700

    Time: 0 ms (100%), Space: 22.2 MB (97.95%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
 class Solution {
 public:
     void solve(TreeNode* &curr, int l, int r, vector<int> &nums) {
