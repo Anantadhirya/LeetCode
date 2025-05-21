772a4452afc875338ace78646c54a195f58ac0af
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-05-21 08:10:34 +0700

    Time: 176 ms (69.46%), Space: 101.9 MB (41.82%) - LeetHub

--- 3393-count-paths-with-the-given-xor-value/3393-count-paths-with-the-given-xor-value.cpp
+++ 3393-count-paths-with-the-given-xor-value/3393-count-paths-with-the-given-xor-value.cpp
@@ -1,3 +1,4 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
 class Solution {
 public:
     int countPathsWithXorValue(vector<vector<int>>& grid, int k) {
