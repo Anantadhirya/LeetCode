727d39c2c959db71b582bfb9f33db0f4e5e181b3
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-05-22 19:14:13 +0700

    Time: 117 ms (66.51%), Space: 222.8 MB (97.35%) - LeetHub

--- 3362-zero-array-transformation-iii/3362-zero-array-transformation-iii.cpp
+++ 3362-zero-array-transformation-iii/3362-zero-array-transformation-iii.cpp
@@ -1,3 +1,5 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 class Solution {
 public:
     int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
