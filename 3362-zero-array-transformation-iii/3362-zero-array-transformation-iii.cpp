a82b2a048d2ac2dba7ab22ad9ef04c2cdab0973e
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-05-22 19:13:40 +0700

    Time: 120 ms (60.48%), Space: 224 MB (94.94%) - LeetHub

---
+++ 3362-zero-array-transformation-iii/3362-zero-array-transformation-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size(), m = queries.size();
+        sort(queries.begin(), queries.end());
+        int curr = 0;
+        vector<int> f(n+1, 0);
+        priority_queue<int> pq;
+        int ans = 0;
+        for(int i = 0, j = 0; i < n; i++) {
+            curr -= f[i];
+            while(j < m && queries[j][0] <= i)
+                pq.push(queries[j++][1]);
+            while(curr < nums[i] && !pq.empty() && i <= pq.top()) {
+                curr++;
+                f[pq.top()+1]++;
+                pq.pop();
+            }
+            if(curr < nums[i]) return -1;
+        }
+        return pq.size();
+    }
+};
