12fffcb75e0b1e2f2a3d2bbd11da4a4e798bd428
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-05-30 16:52:53 +0700

    Time: 12 ms (80.5%), Space: 98.5 MB (98.83%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        const int inf = INT_MAX;
+        int n = edges.size();
+        pair<int, int> ans = {inf, -1};
+        vector<int> dist(n, inf);
+        vector<bool> vis(n, 0);
+        for(int i = node1, d = 0; i != -1 && dist[i] == inf; i = edges[i], d++) {
+            dist[i] = d;
+        }
+        for(int i = node2, d = 0; i != -1 && !vis[i]; vis[i] = 1, i = edges[i], d++) {
+            ans = min(ans, {max(d, dist[i]), i});
+        }
+        return ans.second;
+    }
+};
