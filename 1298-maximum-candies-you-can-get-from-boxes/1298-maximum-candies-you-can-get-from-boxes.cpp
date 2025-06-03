731555bfe3a33aed703efa19f1d3fb184e4506c0
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-06-03 16:59:25 +0700

    Time: 3 ms (68.75%), Space: 44.6 MB (43.27%) - LeetHub

--- 1298-maximum-candies-you-can-get-from-boxes/1298-maximum-candies-you-can-get-from-boxes.cpp
+++ 1298-maximum-candies-you-can-get-from-boxes/1298-maximum-candies-you-can-get-from-boxes.cpp
@@ -1,28 +1,27 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 class Solution {
 public:
     int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
         int n = status.size();
         vector<int> owned(n, 0);
-        vector<bool> vis(n, 0);
         queue<int> q;
         int ans = 0, curr;
         for(const auto &i: initialBoxes) {
-            owned[i] = 1;
             if(status[i] == 1) q.push(i);
+            else owned[i] = 1;
         }
         while(!q.empty()) {
             curr = q.front();
             q.pop();
-            if(vis[curr]) continue;
-            vis[curr] = 1;
             ans += candies[curr];
             for(int &to: containedBoxes[curr]) {
                 owned[to] = 1;
-                if(status[to] == 1) q.push(to);
+                if(status[to] == 1) q.push(to), owned[to] = 0;
             }
             for(int &to: keys[curr]) {
                 status[to] = 1;
-                if(owned[to] == 1) q.push(to);
+                if(owned[to] == 1) q.push(to), owned[to] = 0;
             }
         }
         return ans;
