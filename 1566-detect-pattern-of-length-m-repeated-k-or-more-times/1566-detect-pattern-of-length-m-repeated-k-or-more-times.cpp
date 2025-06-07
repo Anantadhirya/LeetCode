1ccbf39d0a189231999e558202585b0cb2794ce9
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-06-07 12:23:27 +0700

    Time: 0 ms (100%), Space: 11 MB (62.12%) - LeetHub

--- 1566-detect-pattern-of-length-m-repeated-k-or-more-times/1566-detect-pattern-of-length-m-repeated-k-or-more-times.cpp
+++ 1566-detect-pattern-of-length-m-repeated-k-or-more-times/1566-detect-pattern-of-length-m-repeated-k-or-more-times.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-    const int key = 461;
-    const int MOD = 1e9 + 7;
     bool containsPattern(vector<int>& arr, int m, int k) {
-        int keyM = 1;
-        int n = arr.size();
-        for(int i = 0; i < m; i++) {
-            keyM = ((long long)keyM * key) % MOD;
-        }
-        vector<int> v(n, 0);
-        v[0] = arr[0];
-        for(int i = 1; i < n; i++) {
-            v[i] = ((long long)key * v[i-1] % MOD + arr[i]) % MOD;
-        }
-        for(int i = n-1; i >= m; i--) {
-            v[i] = ((long long)v[i] - (long long)v[i-m] * keyM % MOD + MOD) % MOD;
-        }
-        for(int i = m-1; i < n; i++) {
-            arr[i] = 1 + (i-m >= m-1 && v[i-m] == v[i] ? arr[i-m] : 0);
-            if(arr[i] >= k) return 1;
+        int cnt = 0, n = arr.size() - m;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] != arr[i+m]) cnt = 0;
+            else cnt++;
+
+            if(cnt >= (k-1)*m) return 1;
         }
         return 0;
     }
