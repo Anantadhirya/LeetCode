15758ac63956019775758757f4233d934a069f91
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-06-08 11:03:13 +0700

    Time: 77 ms (72.98%), Space: 63.3 MB (75.76%) - LeetHub

---
+++ 2407-longest-increasing-subsequence-ii/2407-longest-increasing-subsequence-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int query(int l, int r, int sl, int sr, int si, vector<int> &st) {
+        if(sr < l || r < sl) return 0;
+        if(l <= sl && sr <= r) return st[si];
+        int sm = (sl+sr)/2;
+        return max(query(l, r, sl, sm, 2*si+1, st), query(l, r, sm+1, sr, 2*si+2, st));
+    }
+    void update(int idx, int val, int sl, int sr, int si, vector<int> &st) {
+        st[si] = max(st[si], val);
+        if(sl == sr) return;
+        int sm = (sl+sr)/2;
+        if(idx <= sm) update(idx, val, sl, sm, 2*si+1, st);
+        else update(idx, val, sm+1, sr, 2*si+2, st);
+    }
+    int lengthOfLIS(vector<int>& nums, int k) {
+        int n = nums.size();
+        int mx = *max_element(nums.begin(), nums.end());
+        vector<int> st(4*mx, 0);
+        int ans = 0, dp;
+        for(const int &i: nums) {
+            dp = query(max(1, i-k), i-1, 1, mx, 0, st) + 1;
+            ans = max(ans, dp);
+            update(i, dp, 1, mx, 0, st);
+        }
+        return ans;
+    }
+};
