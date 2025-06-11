adbe194850a0683342dcaa2f1034f0b3201095d0
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-06-11 11:46:09 +0700

    Time: 223 ms (52.56%), Space: 73.6 MB (52.56%) - LeetHub

--- 3445-maximum-difference-between-even-and-odd-frequency-ii/3445-maximum-difference-between-even-and-odd-frequency-ii.cpp
+++ 3445-maximum-difference-between-even-and-odd-frequency-ii/3445-maximum-difference-between-even-and-odd-frequency-ii.cpp
@@ -1,6 +1,9 @@
 const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
 class Solution {
 public:
+    int f(int cnta, int cntb) {
+        return ((cnta&1)<<1) + (cntb&1);
+    }
     void ms(int &a, int b) {
         a = max(a, b);
     }
@@ -9,26 +12,27 @@ public:
         int n = s.size();
         for(auto &i: s) i -= '0';
         int ans = -inf;
-        int cnta, cntb, cnt, idx;
+        int cnta, cntb, prefa, prefb;
+        vector<int> mx(4);
         vector<vector<vector<int>>> pref(n+1, vector<vector<int>>(2, vector<int>(2)));
         for(int a = 0; a <= 4; a++) {
             for(int b = 0; b <= 4; b++) {
                 if(b == a) continue;
-                cnta = cntb = cnt = 0;
-                pref[0][0][0] = 0;
-                pref[0][0][1] = pref[0][1][0] = pref[0][1][1] = -inf;
+                cnta = cntb = prefa = prefb = 0;
+                mx = {-inf, -inf, -inf, -inf};
                 for(int r = 0, l = 0; r < n; r++) {
-                    if(s[r] == a) cnta++;
-                    else if(s[r] == b) cntb++, cnt++;
-                    while(cnt >= 2) if(s[l++] == b) cnt--;
+                    cnta += (s[r] == a);
+                    cntb += (s[r] == b);
 
-                    pref[r+1] = pref[r];
-                    ms(pref[r+1][cnta&1][cntb&1], -(cnta-cntb));
-                    // substring i..r have cnt >= 2 if i < l
+                    while(r-l+1 >= k && cntb - prefb >= 2) {
+                        ms(mx[f(prefa, prefb)], -(prefa - prefb));
+                        prefa += (s[l] == a);
+                        prefb += (s[l] == b);
+                        l++;
+                    }
 
-                    idx = min(l-1, r-k+1);
-                    if(idx >= 0 && pref[idx][~cnta&1][cntb&1] != -inf) {
-                        ms(ans, cnta - cntb + pref[idx][~cnta&1][cntb&1]);
+                    if(l-1 >= 0 && mx[f(cnta+1, cntb)] != -inf) {
+                        ms(ans, cnta - cntb + mx[f(cnta+1, cntb)]);
                     }
                 }
             }
