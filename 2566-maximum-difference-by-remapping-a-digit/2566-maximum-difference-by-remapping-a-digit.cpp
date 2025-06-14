c72127cfe26f30ee9b88a63b37494a48298f25e5
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-06-14 08:39:46 +0700

    Time: 0 ms (100%), Space: 8.1 MB (84.91%) - LeetHub

--- 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.cpp
+++ 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.cpp
@@ -1,17 +1,24 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 class Solution {
 public:
     int minMaxDifference(int num) {
-        string s = to_string(num);
-        int n = s.size(), idx1 = 0, idx2 = 0;
+        vector<int> v;
+        while(num) {
+            v.push_back(num%10);
+            num /= 10;
+        }
+        reverse(v.begin(), v.end());
+        int n = v.size(), idx1 = 0, idx2 = 0;
         char c1, c2;
         int mx = 0, mn = 0;
-        while(idx1 < n && s[idx1] == '9') idx1++;
-        while(idx2 < n && s[idx2] == '0') idx2++;
-        c1 = (idx1 == n ? '9' : s[idx1]);
-        c2 = (idx2 == n ? '0' : s[idx2]);
+        while(idx1 < n && v[idx1] == 9) idx1++;
+        while(idx2 < n && v[idx2] == 0) idx2++;
+        c1 = (idx1 == n ? 9 : v[idx1]);
+        c2 = (idx2 == n ? 0 : v[idx2]);
         for(int i = 0; i < n; i++) {
-            mx = 10*mx + (s[i] == c1 ? 9 : s[i]-'0');
-            mn = 10*mn + (s[i] == c2 ? 0 : s[i]-'0');
+            mx = 10*mx + (v[i] == c1 ? 9 : v[i]);
+            mn = 10*mn + (v[i] == c2 ? 0 : v[i]);
         }
         return mx - mn;
     }
