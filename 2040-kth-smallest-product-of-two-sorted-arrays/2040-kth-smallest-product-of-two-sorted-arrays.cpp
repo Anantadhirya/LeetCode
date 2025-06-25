670175ff39d088e810925770dd6cdab8959a12d4
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-06-25 20:58:04 +0700

    Time: 667 ms (11.48%), Space: 97.9 MB (90.33%) - LeetHub

---
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        const long long inf = 1e10;
+        long long l = -inf, r = inf, mid, ans, cnt;
+        int ll, rr, mid2, cnt2, m = nums2.size();
+        while(l <= r) {
+            mid = (l+r)/2;
+            cnt = 0;
+            for(const int &i: nums1) {
+                ll = 0;
+                rr = m-1;
+                cnt2 = 0;
+                while(ll <= rr) {
+                    mid2 = (ll+rr)/2;
+                    if((long long)i * nums2[mid2] <= mid) {
+                        if(i >= 0) cnt2 = mid2 + 1, ll = mid2 + 1;
+                        else cnt2 = m - mid2, rr = mid2 - 1;
+                    } else {
+                        if(i >= 0) rr = mid2 - 1;
+                        else ll = mid2 + 1;
+                    }
+                }
+                cnt += cnt2;
+            }
+            if(cnt >= k) {
+                ans = mid;
+                r = mid - 1;
+            } else l = mid + 1;
+        }
+        return ans;
+    }
+};
