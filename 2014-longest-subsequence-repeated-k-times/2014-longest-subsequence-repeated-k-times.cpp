77e67f246d733f25aa7f4d287d5d657c6a1f0fe4
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-06-27 19:23:20 +0700

    Time: 689 ms (35.51%), Space: 10.7 MB (94.2%) - LeetHub

--- 2014-longest-subsequence-repeated-k-times/2014-longest-subsequence-repeated-k-times.cpp
+++ 2014-longest-subsequence-repeated-k-times/2014-longest-subsequence-repeated-k-times.cpp
@@ -1,3 +1,5 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 class Solution {
 public:
     void solve(vector<char> &v, int &n, int &k, string &curr, vector<bool> &vis, string &s, string &ans) {
