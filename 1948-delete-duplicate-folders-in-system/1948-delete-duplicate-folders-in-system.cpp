af1f987d88d422259a87b4557fd035b0ea6a2fb8
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-07-20 09:10:15 +0700

    Time: 179 ms (83.1%), Space: 268.9 MB (70.42%) - LeetHub

---
+++ 1948-delete-duplicate-folders-in-system/1948-delete-duplicate-folders-in-system.cpp
@@ -0,0 +1,49 @@
+struct Trie {
+    string s = "";
+    unordered_map<string, Trie*> adj;
+};
+
+class Solution {
+public:
+    void dfs(Trie *curr, map<string, int> &cnt) {
+        if(curr->adj.empty()) return;
+        vector<string> v;
+        for(const auto &[folder, node]: curr->adj) {
+            dfs(node, cnt);
+            v.push_back(folder + "(" + node->s + ")");
+        }
+        sort(v.begin(), v.end());
+        for(const auto &s: v) {
+            curr->s += s;
+        }
+        cnt[curr->s]++;
+    }
+    void dfs2(Trie *curr, map<string, int> &cnt, vector<string> &path, vector<vector<string>> &ans) {
+        if(cnt[curr->s] > 1) return;
+        if(!path.empty()) ans.push_back(path);
+        for(const auto &[folder, node]: curr->adj) {
+            path.push_back(folder);
+            dfs2(node, cnt, path, ans);
+            path.pop_back();
+        }
+    }
+    vector<vector<string>> deleteDuplicateFolder(vector<vector<string>>& paths) {
+        Trie *root = new Trie();
+        Trie *curr;
+        for(const auto &path: paths) {
+            curr = root;
+            for(const auto &folder: path) {
+                if(!curr->adj.count(folder)) {
+                    curr->adj[folder] = new Trie();
+                }
+                curr = curr->adj[folder];
+            }
+        }
+        map<string, int> cnt;
+        dfs(root, cnt);
+        vector<string> path;
+        vector<vector<string>> ans;
+        dfs2(root, cnt, path, ans);
+        return ans;
+    }
+};
