0bcda2795a2fb231f4fdf0baca9e9d9a2e7c68bd
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-07-20 09:10:55 +0700

    Time: 178 ms (83.1%), Space: 269.2 MB (66.2%) - LeetHub

--- 1948-delete-duplicate-folders-in-system/1948-delete-duplicate-folders-in-system.cpp
+++ 1948-delete-duplicate-folders-in-system/1948-delete-duplicate-folders-in-system.cpp
@@ -1,3 +1,5 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 struct Trie {
     string s = "";
     unordered_map<string, Trie*> adj;
@@ -5,7 +7,7 @@ struct Trie {
 
 class Solution {
 public:
-    void dfs(Trie *curr, map<string, int> &cnt) {
+    void dfs(Trie *curr, unordered_map<string, int> &cnt) {
         if(curr->adj.empty()) return;
         vector<string> v;
         for(const auto &[folder, node]: curr->adj) {
@@ -18,7 +20,7 @@ public:
         }
         cnt[curr->s]++;
     }
-    void dfs2(Trie *curr, map<string, int> &cnt, vector<string> &path, vector<vector<string>> &ans) {
+    void dfs2(Trie *curr, unordered_map<string, int> &cnt, vector<string> &path, vector<vector<string>> &ans) {
         if(cnt[curr->s] > 1) return;
         if(!path.empty()) ans.push_back(path);
         for(const auto &[folder, node]: curr->adj) {
@@ -39,7 +41,7 @@ public:
                 curr = curr->adj[folder];
             }
         }
-        map<string, int> cnt;
+        unordered_map<string, int> cnt;
         dfs(root, cnt);
         vector<string> path;
         vector<vector<string>> ans;
