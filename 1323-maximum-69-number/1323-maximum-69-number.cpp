7512a4a10d1a4b9ae043256be250ff08d960deeb
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-08-16 08:23:18 +0700

    Time: 0 ms (100%), Space: 8 MB (71.54%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        for(int i = 1000; i >= 1; i /= 10) {
+            if((num/i)%10 == 6) return num + 3*i;
+        }
+        return num;
+    }
+};
