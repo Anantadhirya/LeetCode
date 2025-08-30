93ce55527c3bba8c1ba44debca457e10facd4c30
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-08-30 09:50:25 +0700

    Time: 0 ms (100%), Space: 14.1 MB (5.2%) - LeetHub

---
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
@@ -0,0 +1,22 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
+class Solution {
+public:
+    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> r(n, 0), c(n, 0);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                r[i] = max(r[i], grid[i][j]);
+                c[j] = max(c[j], grid[i][j]);
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                ans += min(r[i], c[j]) - grid[i][j];
+            }
+        }
+        return ans;
+    }
+};
