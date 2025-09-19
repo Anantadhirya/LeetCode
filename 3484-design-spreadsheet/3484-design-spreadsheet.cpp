e3349f644000878dd9e27dfa5449cd70b6929dfc
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-09-19 09:57:44 +0700

    Time: 68 ms (80.58%), Space: 162.2 MB (66.49%) - LeetHub

---
+++ 3484-design-spreadsheet/3484-design-spreadsheet.cpp
@@ -0,0 +1,42 @@
+class Spreadsheet {
+public:
+    vector<vector<int>> v;
+    Spreadsheet(int rows) {
+        v.assign(26, vector<int>(rows));
+    }
+    
+    void setCell(string cell, int value) {
+        int c = cell[0] - 'A';
+        int r = stoi(cell.substr(1)) - 1;
+        v[c][r] = value;
+    }
+    
+    void resetCell(string cell) {
+        setCell(cell, 0);
+    }
+
+    int get(string value) {
+        if('A' <= value[0] && value[0] <= 'Z') {
+            int c = value[0] - 'A';
+            int r = stoi(value.substr(1)) - 1;
+            return v[c][r];
+        } else return stoi(value);
+    }
+    
+    int getValue(string formula) {
+        for(int i = formula.size()-1; i >= 1; i--) {
+            if(formula[i] == '+') {
+                return get(formula.substr(1, i-1)) + get(formula.substr(i+1));
+            }
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet* obj = new Spreadsheet(rows);
+ * obj->setCell(cell,value);
+ * obj->resetCell(cell);
+ * int param_3 = obj->getValue(formula);
+ */
