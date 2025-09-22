0a8874c73a2ab80a2c98e5bcd3fafceb252b96fa
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-09-22 09:52:35 +0700

    Time: 240 ms (46.03%), Space: 60.6 MB (56.62%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
-        priority_queue<pair<int, int>> pq;
-        int mx = 0, x;
-        int ans;
-        for(const int &i: nums) {
-            x = i;
-            while(~x&1) x >>= 1;
-            mx = max(mx, x);
-            pq.push({-x, (i&1 ? 2*i : i)});
+        priority_queue<int> pq;
+        int mn = INT_MAX, ans = INT_MAX, x;
+        for(int &i: nums) {
+            i <<= (i&1);
+            mn = min(mn, i);
+            pq.push(i);
         }
-        while(-pq.top().first <= (min(mx,pq.top().second)>>1)) {
-            x = -pq.top().first;
-            while(x <= (min(mx,pq.top().second)>>1)) x <<= 1;
-            pq.push({-x, pq.top().second});
-            pq.pop(); 
-        }
-        ans = mx + pq.top().first;
-        while(1) {
-            x = -pq.top().first;
-            if(x <= (pq.top().second>>1)) {
-                x <<= 1;
-                mx = max(mx, x);
-                pq.push({-x, pq.top().second});
-                pq.pop();
-            } else break;
-            while(-pq.top().first <= (min(mx, pq.top().second)>>1)) {
-                pq.push({2*pq.top().first, pq.top().second});
-                pq.pop();
-            }
-            ans = min(ans, mx + pq.top().first);
+        ans = pq.top() - mn;
+        while(pq.top() % 2 == 0) {
+            x = pq.top();
+            pq.pop();
+
+            x >>= 1;
+            mn = min(mn, x);
+            pq.push(x);
+            ans = min(ans, pq.top() - mn);
         }
         return ans;
     }
