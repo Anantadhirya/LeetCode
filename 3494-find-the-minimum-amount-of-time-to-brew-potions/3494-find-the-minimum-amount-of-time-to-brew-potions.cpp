83b816aca054868ce33af65f5d0b48890a15b3f7
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-10-09 12:42:43 +0700

    Time: 431 ms (59.09%), Space: 45.7 MB (31.82%) - LeetHub

--- 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.cpp
+++ 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.cpp
@@ -1,3 +1,5 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 class Solution {
 public:
     long long minTime(vector<int>& skill, vector<int>& mana) {
