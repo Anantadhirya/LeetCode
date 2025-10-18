64d58539e4bcf8dfc0020663b5690139dab2fde0
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-10-18 09:27:28 +0700

    Time: 68 ms (99.68%), Space: 97.8 MB (46.06%) - LeetHub

--- 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.cpp
@@ -1,16 +1,13 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 class Solution {
 public:
     int maxDistinctElements(vector<int>& nums, int k) {
         int ans = 0, cur = INT_MIN;
         sort(nums.begin(), nums.end());
-        for(auto i: nums) {
-            if(cur+1 < i-k) {
-                cur = i-k;
-                ans++;
-            } else if(cur+1 <= i+k) {
-                cur++;
-                ans++;
-            }
+        for(const int &i: nums) {
+            if(cur+1 < i-k) cur = i-k, ans++;
+            else if(cur+1 <= i+k) cur++, ans++;
         }
         return ans;
     }
