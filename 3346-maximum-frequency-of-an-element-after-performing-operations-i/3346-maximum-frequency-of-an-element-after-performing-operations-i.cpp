8ab707cf8f4311795b2ca136c7e5af4db1356ff0
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-10-21 10:46:30 +0700

    Time: 47 ms (84.47%), Space: 90.6 MB (100%) - LeetHub

--- 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,11 +1,12 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
         int ans = 0;
-        int mx = nums.back();
-        for(int i = 0, j = 0, l = 0, r = 0, val = 1; val <= mx; val++) {
+        for(int i = 0, j = 0, l = 0, r = 0, val = nums[0]; val <= nums.back(); val++) {
             while(i < n && nums[i] < val) i++;
             while(j < n && nums[j] <= val) j++;
             while(l < n && nums[l] < val - k) l++;
