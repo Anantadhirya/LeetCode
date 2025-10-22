c43a7c24acde8cd04fe72a0e6c4563a7b42626b1
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-10-22 11:26:12 +0700

    Time: 158 ms (89.1%), Space: 104.7 MB (88.63%) - LeetHub

--- 3347-maximum-frequency-of-an-element-after-performing-operations-ii/3347-maximum-frequency-of-an-element-after-performing-operations-ii.cpp
+++ 3347-maximum-frequency-of-an-element-after-performing-operations-ii/3347-maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -1,3 +1,5 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
