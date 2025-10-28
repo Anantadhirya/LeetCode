9bcb131c0acfe813a51daa9c9ce54018950259ae
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-10-28 08:18:20 +0700

    Time: 4 ms (68.35%), Space: 22 MB (67.89%) - LeetHub

---
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countValidSelections(vector<int>& nums) {
+        int sm = 0, ans = 0, tmp = 0;
+        for(auto i: nums) {
+            sm += i;
+        }
+        for(auto i: nums) {
+            tmp += i;
+            if(i != 0) continue;
+            if(tmp * 2 == sm) ans += 2;
+            if(abs(tmp * 2 - sm) == 1) ans += 1;
+        }
+        return ans;
+    }
+};
