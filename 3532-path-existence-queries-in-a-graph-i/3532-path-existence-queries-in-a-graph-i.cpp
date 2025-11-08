65f9300313a64d0d6e6d10511d5e3a3091b027d4
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-11-08 09:11:34 +0700

    Time: 47 ms (43.83%), Space: 235.8 MB (38.29%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<pair<int, int>> v(n);
-        vector<int> pref(n);
+        vector<int> c(n);
         vector<bool> ans;
-        int a, b;
-        for(int i = 0; i < n; i++) {
-            pref[i] = (i-1 >= 0 ? pref[i-1] : 0) + (i+1 < n ? nums[i+1] - nums[i] <= maxDiff : 0);
+        c[0] = 0;
+        for(int i = 1; i < n; i++) {
+            c[i] = c[i-1] + (nums[i] - nums[i-1] > maxDiff);
         }
         for(auto i: queries) {
-            a = i[0];
-            b = i[1];
-            if(a > b) swap(a, b);
-            ans.push_back((b-1 >= 0 ? pref[b-1] : 0) - (a-1 >= 0 ? pref[a-1] : 0) == b-a);
+            ans.push_back(c[i[0]] == c[i[1]]);
         }
         return ans;
     }
