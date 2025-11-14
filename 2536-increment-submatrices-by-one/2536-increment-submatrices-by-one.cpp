5cc17931f3ed68df005637cd0cd0fad4f627e288
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2025-11-14 09:50:54 +0700

    Time: 39 ms (52.04%), Space: 91.4 MB (37.62%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -1,3 +1,5 @@
+const auto init = []() { ios_base::sync_with_stdio(false); cin.tie(0); return nullptr; }();
+
 class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
