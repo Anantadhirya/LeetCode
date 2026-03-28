197359a14258a983aab0f8ece0306bba43fc9825
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2026-03-28 10:20:59 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.cpp
+++ 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.cpp
@@ -1,37 +1,14 @@
 class Solution {
 public:
-    int fp(int i, vector<int> &par) {
-        if(par[i] == i) return i;
-        return par[i] = fp(par[i], par);
-    }
-    void join(int u, int v, vector<int> &par) {
-        u = fp(par[u], par);
-        v = fp(par[v], par);
-        par[u] = v;
-    }
     string findTheString(vector<vector<int>>& lcp) {
-        int n = lcp.size();
-        vector<int> par(n);
-        vector<int> c(n, -1);
-        int t = 0;
+        int n = lcp.size(), t = 0;
         string ans(n, ' ');
-        for(int i = 0; i < n; i++) par[i] = i;
         
         for(int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++) {
-                for(int k = 0; k < lcp[i][j]; k++) {
-                    if(j+k >= n) return "";
-                    join(i+k, j+k, par);
-                }
-            }
-        }
-        for(int i = 0, j; i < n; i++) {
-            j = fp(i, par);
-            if(c[j] == -1) {
-                if(t >= 26) return "";
-                c[j] = t++;
-            }
-            ans[i] = 'a'+c[j];
+            if(ans[i] != ' ') continue;
+            ans[i] = 'a' + t;
+            if(t++ >= 26) return ""; 
+            for(int j = 0; j < n; j++) if(lcp[i][j]) ans[j] = ans[i];
         }
         // cout << ans << "\n";
         for(int i = n-1; i >= 0; i--) {
