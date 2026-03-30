6f4c1d8e9b3ea7fa77c7e6b864d507a78eafbda9
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2026-03-30 08:27:40 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    pair<string, string> split(string &s) {
-        string ret[2];
-        int n = s.size();
-        for(int i = 0; i < n; i++) {
-            ret[i&1] += s[i];
-        }
-        return {ret[0], ret[1]};
-    }
     bool checkStrings(string s1, string s2) {
-        pair<string, string> p1 = split(s1), p2 = split(s2);
-        sort(p1.first.begin(), p1.first.end());
-        sort(p1.second.begin(), p1.second.end());
-        sort(p2.first.begin(), p2.first.end());
-        sort(p2.second.begin(), p2.second.end());
-        return p1 == p2;
+        int n = s1.size();
+        vector<vector<int>> cnt(2, vector<int>(26, 0));
+        for(int i = 0; i < n; i++) {
+            cnt[i&1][s1[i]-'a']++;
+            cnt[i&1][s2[i]-'a']--;
+        }
+        for(int i = 0; i < 26; i++) {
+            if(cnt[0][i] != 0 || cnt[1][i] != 0) return 0;
+        }
+        return 1;
     }
 };
