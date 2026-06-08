050442f97463c95928aa6e61857c7328de6afc07
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2026-06-08 09:20:36 +0700

    Time: 76 ms (53.53%), Space: 124.3 MB (53.67%) - LeetHub

---
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        const int inf = INT_MAX;
+        vector<vector<int>> dp(n, vector<int>(2, inf));
+        dp[n-1][0] = 0;
+        dp[n-1][1] = 1;
+        for(int i = n-2; i >= 0; i--) {
+            if(nums1[i] < nums1[i+1] && nums2[i] < nums2[i+1]) {
+                dp[i][0] = min(dp[i][0], dp[i+1][0]);
+                dp[i][1] = min(dp[i][1], dp[i+1][1] + 1);
+            }
+            if(nums1[i] < nums2[i+1] && nums2[i] < nums1[i+1]) {
+                dp[i][0] = min(dp[i][0], dp[i+1][1]);
+                dp[i][1] = min(dp[i][1], dp[i+1][0] + 1);
+            }
+        }
+        return min(dp[0][0], dp[0][1]);
+    }
+};
